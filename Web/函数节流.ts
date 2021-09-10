de7e9c96ded3fe7c4d268e489093426712712cf2
HarryXiong24 <harryxiong24@gmail.com>
2021-09-10 16:17:40 +0800

    9.10

--- Web/函数节流.ts
+++ Web/函数节流.ts
@@ -2,7 +2,7 @@
 
 // 在函数需要频繁触发时，函数触发一次后，只有大于设定的执行周期后才会第二次执行改函数
 
-function throttle(callback: Function, delay: number, immediate: boolean) {
+export function throttle(callback: Function, delay: number, immediate: boolean) {
   let flag: boolean = true;
   return function(this: unknown, ...args: any) {
     if (immediate) {
