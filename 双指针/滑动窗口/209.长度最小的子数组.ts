324d90dbad4e7d752f0f61792e55421bd351db71
HarryXiong24 <harryxiong24@gmail.com>
2021-09-23 22:26:09 +0800

    9.23

--- 双指针/滑动窗口/209.长度最小的子数组.ts
+++ 双指针/滑动窗口/209.长度最小的子数组.ts
@@ -5,6 +5,10 @@
  * 找出该数组中满足其和 ≥ target 的长度最小的连续子数组 [numsl, numsl+1, ..., numsr-1, numsr]
  * 并返回其长度
  * 如果不存在符合条件的子数组，返回 0
+ * 示例：
+ * 输入：target = 7, nums = [2,3,1,2,4,3]
+ * 输出：2
+ * 解释：子数组 [4,3] 是该条件下的长度最小的子数组。
  */
 
 export function minSubArrayLen(target: number, nums: number[]): number {
