9a6078911c1c6bd8ec3d93d2b273445e0eee9d13
HarryXiong24 <harryxiong24@gmail.com>
2021-10-28 21:56:34 +0800

    10.28

---
+++ Typescript/1.Hello World.ts
@@ -0,0 +1,12 @@
+// 1 Hello World
+
+/**
+ * 在这个挑战中，你需要修改下方的代码使得测试通过（使其没有类型错误）
+ * 期望是一个 string 类型
+ * type HelloWorld = any
+ * 你需要使得如下这行不会抛出异常
+ * type test = Expect<Equal<HelloWorld, string>>
+ */
+
+// solution
+type HelloWorld = string;
