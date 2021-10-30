0eb522b355a9cb0c0baf66a30f8d600732397cc9
HarryXiong24 <harryxiong24@gmail.com>
2021-10-30 22:21:19 +0800

    10.30

--- test/practiceType.ts
+++ test/practiceType.ts
@@ -9,3 +9,10 @@ export type TupleToObject<T extends any[]> = {
 
 // First of Array
 export type First<T extends any[]> = T extends [infer K, ...any] ? K : never;
+export type First2<T extends any[]> = T['length'] extends 0 ? never : T[0];
+
+// Last of Array
+export type Last<T extends any[]> = T extends [...any, infer K] ? K : never;
+
+// Length of Tuple
+export type Length<T extends any[]> = T['length'];
