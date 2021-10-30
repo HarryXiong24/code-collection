cb13a3ee536b9231862856152cc9adc75e0f3ca5
HarryXiong24 <harryxiong24@gmail.com>
2021-10-30 22:11:25 +0800

    10.30

---
+++ test/basicType.ts
@@ -0,0 +1,31 @@
+// Pick
+export type MyPick<T, K extends keyof T> = {
+  [P in K]: T[P];
+};
+
+// Exclude
+export type MyExclude<T, K> = T extends K ? never : T;
+
+// Omit
+export type MyOmit<T, K extends keyof T> = {
+  [P in MyExclude<keyof T, K>]: T[P];
+};
+
+// Readonly
+export type MyReadonly<T> = {
+  readonly [P in keyof T]: T[P];
+};
+
+// Parameters
+export type MyParameters<T extends (...args: any) => any> = T extends (
+  ...args: infer K
+) => any
+  ? K
+  : never;
+
+// ReturnType
+export type MyReturnType<T extends (...args: any) => any> = T extends (
+  ...args: any
+) => infer K
+  ? K
+  : never;
