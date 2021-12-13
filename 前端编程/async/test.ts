8a2191b3a1c93f9ad443144a5f3a5731de5f5a6d
harryxiong <harryxiong24@gmail.com>
2021-12-13 11:18:13 +0800

    12.13

---
+++ 前端编程/async/test.ts
@@ -0,0 +1,14 @@
+function* generate() {
+  let a = 1;
+  console.log(a);
+  yield (a = 2);
+  console.log(a);
+  yield (a = 3);
+  console.log(a);
+}
+
+let gen = generate();
+let res1 = gen.next();
+console.log(res1);
+let res2 = gen.next();
+console.log(res2);
