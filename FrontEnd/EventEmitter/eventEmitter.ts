002505d4cf8e2b9b8be5544c0543153d28818b16
harryxiong <harryxiong24@gmail.com>
2021-12-14 10:38:56 +0800

    12.14

--- FrontEnd/EventEmitter/eventEmitter.ts
+++ FrontEnd/EventEmitter/eventEmitter.ts
@@ -1,17 +1,20 @@
-export type ListenerFunc = () => any;
-export interface ListenerObj {
+type ListenerFunc = () => any;
+
+interface Listener {
   listener: ListenerFunc;
+  once: boolean;
 }
-export type Listener = ListenerFunc | ListenerObj;
+
+interface ListenersContainer {
+  [propName: string]: Listener[];
+}
 
 /**
  * 判断是否为合法的 listener
  */
-function isValidListener(listener: Listener) {
+function isValidListener(listener: ListenerFunc) {
   if (typeof listener === 'function') {
     return true;
-  } else if (listener && typeof listener === 'object') {
-    isValidListener(listener.listener);
   } else {
     return false;
   }
@@ -20,12 +23,11 @@ function isValidListener(listener: Listener) {
 /**
  * 根据 listener name 查找并返回对应的 listener
  */
-function indexOfListener(listener_list: ListenerObj[], listener: Listener): number {
+function indexOfListener(listener_list: Listener[], listener: ListenerFunc): number {
   let result: number = -1;
-  const listener_copy: ListenerFunc = typeof listener === 'object' ? listener.listener : listener;
 
   for (let i = 0; i < listener_list.length; i++) {
-    if (listener_list[i].listener === listener_copy) {
+    if (listener_list[i].listener === listener) {
       result = i;
       break;
     }
@@ -33,3 +35,42 @@ function indexOfListener(listener_list: ListenerObj[], listener: Listener): numb
 
   return result;
 }
+
+class EventEmitter {
+  // 存放所有的监听时间
+  public _events: ListenersContainer = {};
+
+  // 添加事件
+  on(eventName: string, listener: ListenerFunc) {
+    if (!eventName || !listener) {
+      return;
+    }
+
+    if (!isValidListener(listener)) {
+      throw new TypeError('Listener must be a function!');
+    }
+
+    const listeners = this._events[eventName] || [];
+
+    // 不重复添加事件
+    if (indexOfListener(listeners, listener) === -1) {
+      listeners.push({
+        listener: listener,
+        once: false,
+      });
+    } else {
+      throw new TypeError('Cannot add the same function repeatedly!');
+    }
+
+    // 将 listeners 数组归还到 this._events[eventName]
+    this._events[eventName] = listeners;
+  }
+}
+
+// test
+let emitter = new EventEmitter();
+emitter.on('num1', () => {
+  console.log('1');
+});
+emitter.on('num2', () => {});
+console.log(emitter._events);
