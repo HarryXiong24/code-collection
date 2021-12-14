f5c4334ef9d22f23b99eaeb77624d7e286b8ca6b
harryxiong <harryxiong24@gmail.com>
2021-12-14 11:10:30 +0800

    12.14

--- FrontEnd/EventEmitter/eventEmitter.ts
+++ FrontEnd/EventEmitter/eventEmitter.ts
@@ -40,12 +40,15 @@ class EventEmitter {
   // 存放所有的监听时间
   public _events: ListenersContainer = {};
 
-  // 添加事件
-  on(eventName: string, listener: ListenerFunc) {
-    if (!eventName || !listener) {
-      return;
-    }
+  constructor() {}
 
+  /**
+   * 添加事件
+   * @param  {String} eventName 事件名称
+   * @param  {Function} listener 监听器函数
+   * @return {Object} 可链式调用
+   */
+  on(eventName: string, listener: ListenerFunc) {
     if (!isValidListener(listener)) {
       throw new TypeError('Listener must be a function!');
     }
@@ -64,13 +67,97 @@ class EventEmitter {
 
     // 将 listeners 数组归还到 this._events[eventName]
     this._events[eventName] = listeners;
+
+    return this;
+  }
+
+  /**
+   * 添加事件，该事件只能被执行一次
+   * @param  {String} eventName 事件名称
+   * @param  {Function} listener 监听器函数
+   * @return {Object} 可链式调用
+   */
+  once(eventName: string, listener: ListenerFunc) {
+    if (!isValidListener(listener)) {
+      throw new TypeError('Listener must be a function!');
+    }
+
+    const listeners = this._events[eventName] || [];
+
+    // 不重复添加事件
+    if (indexOfListener(listeners, listener) === -1) {
+      listeners.push({
+        listener: listener,
+        once: true,
+      });
+    } else {
+      throw new TypeError('Cannot add the same function repeatedly!');
+    }
+
+    // 将 listeners 数组归还到 this._events[eventName]
+    this._events[eventName] = listeners;
+
+    return this;
+  }
+
+  /**
+   * 删除某一个类型的所有事件或者所有事件
+   * @param  {String[]} eventName 事件名称
+   * @return {Object} 可链式调用
+   */
+  off(eventName: string) {
+    const listeners = this._events[eventName];
+
+    if (!listeners) {
+      return this;
+    }
+
+    Reflect.deleteProperty(this._events, eventName);
+
+    return this;
+  }
+
+  /**
+   * 删除指定的事件
+   * @param  {String} eventName 事件名称
+   * @param  {Function} listener 监听器函数
+   * @return {Object} 可链式调用
+   */
+  offItem(eventName: string, listener: ListenerFunc) {
+    if (!isValidListener(listener)) {
+      throw new TypeError('Listener must be a function!');
+    }
+
+    const listeners = this._events[eventName];
+    if (!listeners) {
+      return this;
+    }
+
+    let index: number = indexOfListener(listeners, listener);
+
+    if (index !== -1) {
+      // 处理 listeners
+      listeners.splice(index, 1);
+    }
+
+    return this;
   }
 }
 
-// test
-let emitter = new EventEmitter();
-emitter.on('num1', () => {
-  console.log('1');
-});
+//tests
+const emitter = new EventEmitter();
+
+const func1 = () => {};
+
+emitter.on('num1', func1);
+
 emitter.on('num2', () => {});
+emitter
+  .once('num2', () => {})
+  ?.on('num2', () => {})
+  ?.once('num2', () => {})
+  ?.on('num3', () => {});
+
+emitter.offItem('num1', func1)?.off('num4')?.off('num3');
+
 console.log(emitter._events);
