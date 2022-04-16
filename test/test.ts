39d5e08d2d2172b952431acb84e8b83fd6a1f104
HarryXiong24 <harryxiong24@gmail.com>
2022-04-16 22:46:11 +0800

    4.16

--- test/test.ts
+++ test/test.ts
@@ -1,9 +1,20 @@
-const arr = [1, 2, 3, 4];
+// const arr = [1, 2, 3, 4];
 
-for (let item of arr) {
-  if (item === 3) {
-    console.log('Break');
-    break;
-  }
-  console.log(item);
+// for (let item of arr) {
+//   if (item === 3) {
+//     console.log('Break');
+//     break;
+//   }
+//   console.log(item);
+// }
+
+function* generator() {
+  console.log('111');
+  yield 123 + 789;
 }
+
+const fun = generator();
+
+console.log(fun.next());
+fun.next();
+fun.next();
