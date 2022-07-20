83500700a8ca68a5d75bc716539f0c722d1682ad
harryxiong <harryxiong24@gmail.com>
2022-07-20 18:13:40 +0800

    7.20

---
+++ NodeJS/koa/compose.js
@@ -0,0 +1,78 @@
+// koa 中间件引擎
+export function compose(middleware) {
+  // 中间件必须存放在一个数组
+  if (!Array.isArray(middleware)) {
+    throw new TypeError('Middleware stack must be an array!');
+  }
+
+  return function (ctx, next) {
+    let index = -1;
+
+    return dispatch(0);
+
+    function dispatch(i) {
+      if (i < index) {
+        return Promise.reject(new Error('next() called multiple times'));
+      }
+
+      index = i;
+
+      let fn = middleware[i];
+
+      if (i === middleware.length) {
+        fn = next;
+      }
+
+      if (!fn) {
+        return Promise.resolve();
+      }
+
+      try {
+        return Promise.resolve(
+          fn(ctx, () => {
+            return dispatch(i + 1);
+          })
+        );
+      } catch (err) {
+        return Promise.reject(err);
+      }
+    }
+  };
+}
+
+// 测试
+let middleware = [];
+let context = {
+  data: [],
+};
+
+middleware.push(async (ctx, next) => {
+  console.log('action 001');
+  ctx.data.push(2);
+  await next();
+  console.log('action 006');
+  ctx.data.push(5);
+});
+
+middleware.push(async (ctx, next) => {
+  console.log('action 002');
+  ctx.data.push(2);
+  // await next();
+  console.log('action 005');
+  ctx.data.push(5);
+});
+
+middleware.push(async (ctx, next) => {
+  console.log('action 003');
+  ctx.data.push(2);
+  await next();
+  console.log('action 004');
+  ctx.data.push(5);
+});
+
+const fn = compose(middleware);
+
+fn(context).then(() => {
+  console.log('end');
+  console.log('context = ', context);
+});
---
+++ NodeJS/koa/index.js
@@ -0,0 +1,97 @@
+const http = require('http');
+const Emitter = require('events');
+const compose = require('./compose');
+
+/**
+ * 通用上下文
+ */
+const context = {
+  _body: null,
+
+  get body() {
+    return this._body;
+  },
+
+  set body(val) {
+    this._body = val;
+    this.res.end(this._body);
+  },
+};
+
+class SimpleKoa extends Emitter {
+  constructor() {
+    super();
+    this.middleware = [];
+    this.context = Object.create(context);
+  }
+
+  /**
+   * 服务事件监听
+   * @param {*} args
+   */
+  listen(...args) {
+    const server = http.createServer(this.callback());
+    return server.listen(...args);
+  }
+
+  /**
+   * 注册使用中间件
+   * @param {Function} fn
+   */
+  use(fn) {
+    if (typeof fn === 'function') {
+      this.middleware.push(fn);
+    }
+  }
+
+  /**
+   * 中间件总回调方法
+   */
+  callback() {
+    if (this.listeners('error').length === 0) {
+      this.on('error', this.onerror);
+    }
+
+    const handleRequest = (req, res) => {
+      let context = this.createContext(req, res);
+      let middleware = this.middleware;
+      // 执行中间件
+      compose(middleware)(context).catch((err) => this.onerror(err));
+    };
+    return handleRequest;
+  }
+
+  /**
+   * 异常处理监听
+   * @param {EndOfStreamError} err
+   */
+  onerror(err) {
+    console.log(err);
+  }
+
+  /**
+   * 创建通用上下文
+   * @param {Object} req
+   * @param {Object} res
+   */
+  createContext(req, res) {
+    let context = Object.create(this.context);
+    context.req = req;
+    context.res = res;
+    return context;
+  }
+}
+
+// 测试
+const SimpleKoa = require('./index');
+
+const app = new SimpleKoa();
+const PORT = 3001;
+
+app.use(async (ctx) => {
+  ctx.body = '<p>this is a body</p>';
+});
+
+app.listen(PORT, () => {
+  console.log(`the web server is starting at port ${PORT}`);
+});
