e0ef9165acef5849bad5141f8891970d7e80e1b1
harryxiong24 <harryxiong24@gmail.com>
2022-10-04 11:12:30 +0800

    2022.10.4

---
+++ Algorithm/数组/二维数组/498.对角线遍历 ?/498.对角线遍历.py
@@ -0,0 +1,19 @@
+# 498 对角线遍历
+
+# 给你一个大小为 m x n 的矩阵 mat ，请以对角线遍历的顺序，用一个数组返回这个矩阵中的所有元素。
+
+# 示例 1：
+# 输入：mat = [[1,2,3],[4,5,6],[7,8,9]]
+# 输出：[1,2,4,7,5,3,6,8,9]
+
+# 示例 2：
+# 输入：mat = [[1,2],[3,4]]
+# 输出：[1,2,3,4]
+
+from typing import List
+
+
+class Solution:
+
+    def findDiagonalOrder(self, mat: List[List[int]]) -> List[int]:
+        return []
