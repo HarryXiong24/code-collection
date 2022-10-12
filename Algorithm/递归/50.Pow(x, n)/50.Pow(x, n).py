68c0f5d21696edc0b3a68f141314b696cf1c95cc
harryxiong24 <harryxiong24@gmail.com>
2022-10-12 18:43:35 +0800

    2022.10.12

---
+++ Algorithm/递归/50.Pow(x, n)/50.Pow(x, n).py
@@ -0,0 +1,40 @@
+# 50 Pow(x, n)
+
+# 实现 pow(x, n) ，即计算 x 的 n 次幂函数（即，x^n ）
+
+# 示例 1：
+# 输入：x = 2.00000, n = 10
+# 输出：1024.00000
+
+# 示例 2：
+# 输入：x = 2.10000, n = 3
+# 输出：9.26100
+
+# 示例 3：
+# 输入：x = 2.00000, n = -2
+# 输出：0.25000
+# 解释：2-2 = 1/22 = 1/4 = 0.25
+
+
+class Solution:
+
+    def myPow(self, x: float, n: int) -> float:
+        power = abs(n)
+        res = self.myPositivePow(x, power)
+        if n > 0:
+            return res
+        else:
+            return 1 / res
+
+    def myPositivePow(self, x: float, n: int) -> float:
+        if n == 0:
+            return 1
+        if n == 1:
+            return x
+        return x * self.myPositivePow(x, n - 1)
+
+
+# test
+solution = Solution()
+res = solution.myPow(2, -2)
+print(res)
