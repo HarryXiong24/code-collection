26a6c27a66d17d4d9c8134e15721291b47f9501c
harryxiong24 <harryxiong24@gmail.com>
2023-04-25 20:43:16 +0800

    2023.4.25

--- Algorithm/Aarry/en/1D Array/724. Find Pivot Index/724. Find Pivot Index.ts
+++ Algorithm/Aarry/en/1D Array/724. Find Pivot Index/724. Find Pivot Index.ts
@@ -1,4 +1,4 @@
-// 724 Find Pivot Index
+// 724. Find Pivot Index
 
 // Given an array of integers nums, calculate the pivot index of this array.
 // The pivot index is the index where the sum of all the numbers strictly to the left of the index is equal to the sum of all the numbers strictly to the index's right.
