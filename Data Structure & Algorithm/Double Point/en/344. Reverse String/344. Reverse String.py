a6b986c5e932f0346018e3b2a2adf89cd3b0e27e
harryxiong24 <harryxiong24@gmail.com>
2023-05-02 12:34:13 +0800

    2023.5.2

---
+++ Data Structure & Algorithm/Double Point/en/344. Reverse String/344. Reverse String.py
@@ -0,0 +1,37 @@
+# 344. Reverse String
+
+# Write a function that reverses a string. The input string is given as an array of characters s.
+# You must do this by modifying the input array in-place with O(1) extra memory.
+
+# Example 1:
+# Input: s = ["h","e","l","l","o"]
+# Output: ["o","l","l","e","h"]
+
+# Example 2:
+# Input: s = ["H","a","n","n","a","h"]
+# Output: ["h","a","n","n","a","H"]
+
+from typing import List
+
+
+class Solution:
+
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        left = 0
+        right = len(s) - 1
+        while left < right:
+            temp = s[left]
+            s[left] = s[right]
+            s[right] = temp
+            left = left + 1
+            right = right - 1
+
+
+# test
+string = ["h", "e", "l", "l", "o"]
+solution = Solution()
+solution.reverseString(string)
+print(string)
