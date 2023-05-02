d56b60673ede3106ab1b9d6920a8dff045204f2f
harryxiong24 <harryxiong24@gmail.com>
2023-05-02 11:58:47 +0800

    2023.5.2

--- Algorithm/String/en/14. Longest Common Prefix/tempCodeRunnerFile.ts
+++
@@ -1,7 +0,0 @@
-  // for (let i = 0; i < short.length; i++) {
-  //   for (let j = 0; j < strs.length; j++) {
-  //     if (short[i] !== strs[j]) {
-  //       return i - 1 > 0 ? short.substring(0, i - 1) : '';
-  //     }
-  //   }
-  // }
