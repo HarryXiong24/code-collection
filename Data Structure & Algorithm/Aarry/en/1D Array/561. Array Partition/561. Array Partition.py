a5778dd11126cabd96027af18f6a7e2043946abc
harryxiong24 <harryxiong24@gmail.com>
2023-05-02 12:41:31 +0800

    2023.5.2

---
+++ Data Structure & Algorithm/Aarry/en/1D Array/561. Array Partition/561. Array Partition.py
@@ -0,0 +1,37 @@
+# 561. Array Partition
+
+# Given an integer array nums of 2n integers, group these integers into n pairs (a1, b1), (a2, b2), ..., (an, bn) such that the sum of min(ai, bi) for all i is maximized. Return the maximized sum.
+
+# Example 1:
+# Input: nums = [1,4,3,2]
+# Output: 4
+# Explanation: All possible pairings (ignoring the ordering of elements) are:
+# 1. (1, 4), (2, 3) -> min(1, 4) + min(2, 3) = 1 + 2 = 3
+# 2. (1, 3), (2, 4) -> min(1, 3) + min(2, 4) = 1 + 2 = 3
+# 3. (1, 2), (3, 4) -> min(1, 2) + min(3, 4) = 1 + 3 = 4
+# So the maximum possible sum is 4.
+
+# Example 2:
+# Input: nums = [6,2,6,5,1,2]
+# Output: 9
+# Explanation: The optimal pairing is (2, 1), (2, 5), (6, 6). min(2, 1) + min(2, 5) + min(6, 6) = 1 + 2 + 6 = 9.
+
+from typing import List
+
+
+class Solution:
+
+    def arrayPairSum(self, nums: List[int]) -> int:
+        arr = sorted(nums)
+        sum = 0
+
+        for i in range(0, len(arr), 2):
+            sum = sum + arr[i]
+
+        return sum
+
+
+# test
+solution = Solution()
+res = solution.arrayPairSum([1, 4, 3, 2])
+print(res)
--- Data Structure & Algorithm/Double Point/en/344. Reverse String/344. Reverse String.ts
+++ Data Structure & Algorithm/Double Point/en/344. Reverse String/344. Reverse String.ts
@@ -14,7 +14,7 @@
 /**
  Do not return anything, modify s in-place instead.
  */
-function reverseString(s: string[]): void {
+export function reverseString(s: string[]): void {
   let left = 0;
   let right = s.length - 1;
   while (left < right) {
