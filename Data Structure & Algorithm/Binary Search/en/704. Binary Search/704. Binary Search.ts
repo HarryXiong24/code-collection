e7a9cf6fdff576b5697c03a4470d610fdd04b241
harryxiong24 <harryxiong24@gmail.com>
2023-05-06 11:46:33 +0800

    2023.5.6

---
+++ Data Structure & Algorithm/Binary Search/en/704. Binary Search/704. Binary Search.ts
@@ -0,0 +1,83 @@
+// 704. Binary Search
+
+// Given an array of integers nums which is sorted in ascending order, and an integer target, write a function to search target in nums. If target exists, then return its index. Otherwise, return -1.
+// You must write an algorithm with O(log n) runtime complexity.
+
+// Example 1:
+// Input: nums = [-1,0,3,5,9,12], target = 9
+// Output: 4
+// Explanation: 9 exists in nums and its index is 4
+
+// Example 2:
+// Input: nums = [-1,0,3,5,9,12], target = 2
+// Output: -1
+// Explanation: 2 does not exist in nums so return -1
+
+// Find the Exact Value
+export function search(nums: number[], target: number): number {
+  let left = 0;
+  let right = nums.length - 1;
+
+  while (left <= right) {
+    const mid = left + Math.floor((right - left) / 2);
+    if (nums[mid] < target) {
+      left = mid + 1;
+    } else if (nums[mid] > target) {
+      right = mid - 1;
+    } else {
+      return mid;
+    }
+  }
+
+  return -1;
+}
+
+// Find Upper bound
+export function search1(nums: number[], target: number): number {
+  let left = 0;
+  let right = nums.length - 1;
+
+  while (left <= right) {
+    const mid = left + Math.floor((right - left) / 2);
+    if (nums[mid] <= target) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
+    }
+  }
+
+  if (left > 0 && nums[left - 1] === target) {
+    return left - 1;
+  } else {
+    return -1;
+  }
+}
+
+// Find Lower bound
+export function search2(nums: number[], target: number): number {
+  let left = 0;
+  let right = nums.length - 1;
+
+  while (left <= right) {
+    const mid = left + Math.floor((right - left) / 2);
+    if (nums[mid] >= target) {
+      right = mid - 1;
+    } else {
+      left = mid + 1;
+    }
+  }
+
+  if (left < nums.length && nums[left] === target) {
+    return left;
+  } else {
+    return -1;
+  }
+}
+
+// test
+const res = search([5], 9);
+const res1 = search1([5], 9);
+const res2 = search2([5], 9);
+console.log(res);
+console.log(res1);
+console.log(res2);
---
+++ Data Structure & Algorithm/Docs/en/Binary Search.md
@@ -0,0 +1,38 @@
+# Binary Search
+
+## Introduction
+
+Understand the general concept of Binary Search.
+
+Binary Search is one of the most fundamental and useful algorithms in Computer Science.
+It describes the process of searching for a specific value in an ordered collection.
+
+Terminology used in Binary Search:
+
+Target - the value that you are searching for
+Index - the current location that you are searching
+Left, Right - the indicies from which we use to maintain our search Space
+Mid - the index that we use to apply a condition to determine if we should search left or right
+
+## How do we identify Binary Search?
+
+As mentioned in earlier, Binary Search is an algorithm that divides the search space in 2 after every comparison. Binary Search should be considered every time you need to search for an index or element in a collection. If the collection is unordered, we can always sort it first before applying Binary Search.
+
+## 3 Parts of a Successful Binary Search
+
+Binary Search is generally composed of 3 main sections:
+
+- Pre-processing - Sort if collection is unsorted.
+- Binary Search - Using a loop or recursion to divide search space in half after each comparison.
+- Post-processing - Determine viable candidates in the remaining space.
+
+## 3 Templates for Binary Search
+
+When we first learned Binary Search, we might struggle. We might study hundreds of Binary Search problems online and each time we looked at a developer's code, it seemed to be implemented slightly differently. Although each implementation divided the problem space in 1/2 at each step, one had numerous questions:
+
+Why was it implemented slightly differently?
+What was the developer thinking?
+Which way was easier?
+Which way is better?
+
+After many failed attempts and lots of hair-pulling, we found 3 main templates for Binary Search.
