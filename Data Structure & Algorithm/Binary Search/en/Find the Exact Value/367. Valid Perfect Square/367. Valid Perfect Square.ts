c60264e417cb1c8ec11450bebfc6fccae6a3bbe8
harryxiong24 <harryxiong24@gmail.com>
2023-05-09 13:11:48 +0800

    2023.5.9

---
+++ Data Structure & Algorithm/Binary Search/en/Find the Exact Value/367. Valid Perfect Square/367. Valid Perfect Square.ts
@@ -0,0 +1,37 @@
+// 367. Valid Perfect Square
+
+// Given a positive integer num, return true if num is a perfect square or false otherwise.
+// A perfect square is an integer that is the square of an integer. In other words, it is the product of some integer with itself.
+// You must not use any built-in library function, such as sqrt.
+
+// Example 1:
+// Input: num = 16
+// Output: true
+// Explanation: We return true because 4 * 4 = 16 and 4 is an integer.
+
+// Example 2:
+// Input: num = 14
+// Output: false
+// Explanation: We return false because 3.742 * 3.742 = 14 and 3.742 is not an integer.
+
+export function isPerfectSquare(num: number): boolean {
+  let left = 0;
+  let right = Math.floor(num / 2) + 1;
+
+  while (left <= right) {
+    const mid = left + Math.floor((right - left) / 2);
+    if (mid * mid < num) {
+      left = mid + 1;
+    } else if (mid * mid > num) {
+      right = mid - 1;
+    } else {
+      return true;
+    }
+  }
+
+  return false;
+}
+
+// test
+const res = isPerfectSquare(14);
+console.log(res);
