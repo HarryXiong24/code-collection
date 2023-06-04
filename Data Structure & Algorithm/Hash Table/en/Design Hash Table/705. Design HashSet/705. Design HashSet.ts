bb04a442e587c19dda83d348a4db3a0e6b5bd8a8
harryxiong24 <harryxiong24@gmail.com>
2023-06-04 16:18:56 +0800

    2023.6.4

---
+++ Data Structure & Algorithm/Hash Table/en/Design Hash Table/705. Design HashSet/705. Design HashSet.ts
@@ -0,0 +1,57 @@
+// 705. Design HashSet
+
+// Design a HashSet without using any built-in hash table libraries.
+
+// Implement MyHashSet class:
+
+// void add(key) Inserts the value key into the HashSet.
+// bool contains(key) Returns whether the value key exists in the HashSet or not.
+// void remove(key) Removes the value key in the HashSet. If key does not exist in the HashSet, do nothing.
+
+// Example 1:
+// Input
+// ["MyHashSet", "add", "add", "contains", "contains", "add", "contains", "remove", "contains"]
+// [[], [1], [2], [1], [3], [2], [2], [2], [2]]
+// Output
+// [null, null, null, true, false, null, true, null, false]
+// Explanation
+// MyHashSet myHashSet = new MyHashSet();
+// myHashSet.add(1);      // set = [1]
+// myHashSet.add(2);      // set = [1, 2]
+// myHashSet.contains(1); // return True
+// myHashSet.contains(3); // return False, (not found)
+// myHashSet.add(2);      // set = [1, 2]
+// myHashSet.contains(2); // return True
+// myHashSet.remove(2);   // set = [1]
+// myHashSet.contains(2); // return False, (already removed)
+
+// use object to simulate set
+export class MyHashSet {
+  hashSet: Record<string, any>;
+
+  constructor() {
+    this.hashSet = {};
+  }
+
+  add(key: number): void {
+    if (!this.hashSet[key]) {
+      this.hashSet[key] = key;
+    }
+  }
+
+  remove(key: number): void {
+    Reflect.deleteProperty(this.hashSet, key);
+  }
+
+  contains(key: number): boolean {
+    return Reflect.has(this.hashSet, key);
+  }
+}
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * var obj = new MyHashSet()
+ * obj.add(key)
+ * obj.remove(key)
+ * var param_3 = obj.contains(key)
+ */
