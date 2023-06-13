517675cf218433bda3678c918a95d5b26edd4cdc
harryxiong24 <harryxiong24@gmail.com>
2023-06-13 15:33:27 +0800

    2023.6.13

--- test/test.ts
+++ test/test.ts
@@ -19,17 +19,23 @@
 // fun.next();
 // fun.next();
 
-const example = new Promise((resolve) => {
-  console.log('aaa');
-  resolve('bbb');
-});
+// const example = new Promise((resolve) => {
+//   console.log('aaa');
+//   resolve('bbb');
+// });
 
-example.then((str) => {
-  console.log(str);
-});
+// example.then((str) => {
+//   console.log(str);
+// });
 
-function Test() {
-  console.log('1');
-}
+// function Test() {
+//   console.log('1');
+// }
 
-console.log(Test);
+// console.log(Test);
+
+const text =
+  '这是一段包含链接的文本，链接地址是：https://www.google.com 和 http://www.baidu.com ，另外还有一个链接：https://github.com/Raoul1996\n';
+const regex = /(https?:\/\/\S+)(?=\s|$)/gi;
+const links = text.match(regex);
+console.log(links); // ["https://www.google.com ", "http://www.baidu.com，", "https://github.com/Raoul1996"]
