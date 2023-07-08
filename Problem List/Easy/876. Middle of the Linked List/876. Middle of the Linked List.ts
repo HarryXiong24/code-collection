9ae54f5edfa75c8a1111e57db11692bb0ca3ad3f
harryxiong24 <harryxiong24@gmail.com>
2023-07-08 11:27:32 +0800

    2023.7.8

---
+++ Problem List/Easy/876. Middle of the Linked List/876. Middle of the Linked List.ts
@@ -0,0 +1,46 @@
+// 876. Middle of the Linked List
+
+// Given the head of a singly linked list, return the middle node of the linked list.
+
+// If there are two middle nodes, return the second middle node.
+
+// Example 1:
+// Input: head = [1,2,3,4,5]
+// Output: [3,4,5]
+// Explanation: The middle node of the list is node 3.
+
+// Example 2:
+// Input: head = [1,2,3,4,5,6]
+// Output: [4,5,6]
+// Explanation: Since the list has two middle nodes with values 3 and 4, we return the second one.
+
+// Definition for singly-linked list.
+class ListNode {
+  val: number;
+  next: ListNode | null;
+  constructor(val?: number, next?: ListNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.next = next === undefined ? null : next;
+  }
+}
+
+// Fast and Slow Pointer
+// Time Complexity: O(N)
+// Space Complexity: O(1)
+export function middleNode(head: ListNode | null): ListNode | null {
+  if (!head) {
+    return null;
+  }
+
+  let slow: ListNode | null = head;
+  let fast: ListNode | null = head;
+
+  // When traversing the list with a pointer slow, make another pointer fast that traverses twice as fast.
+  // When fast reaches the end of the list, slow must be in the middle.
+  while (fast && fast.next) {
+    slow = slow!.next;
+    fast = fast!.next!.next;
+  }
+
+  return slow;
+}
