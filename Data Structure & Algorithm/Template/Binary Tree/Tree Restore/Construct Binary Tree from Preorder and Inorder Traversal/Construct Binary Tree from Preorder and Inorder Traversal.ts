dcfd904dbef693ac1553b3754666cc61c31e2309
harryxiong24 <harryxiong24@gmail.com>
2023-07-24 11:57:31 +0800

    2023.7.24

---
+++ Data Structure & Algorithm/Template/Binary Tree/Tree Restore/Construct Binary Tree from Preorder and Inorder Traversal/Construct Binary Tree from Preorder and Inorder Traversal.ts
@@ -0,0 +1,67 @@
+// 105. Construct Binary Tree from Preorder and Inorder Traversal
+
+// Given two integer arrays preorder and inorder where preorder is the preorder traversal of a binary tree and inorder is the inorder traversal of the same tree, construct and return the binary tree.
+
+// Definition for a binary tree node.
+class TreeNode {
+  val: number;
+  left: TreeNode | null;
+  right: TreeNode | null;
+  constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.left = left === undefined ? null : left;
+    this.right = right === undefined ? null : right;
+  }
+}
+
+export function buildTree(preorder: number[], inorder: number[]): TreeNode | null {
+  const recursive = (preorder: number[], inorder: number[]) => {
+    if (!preorder.length || !inorder.length) {
+      return null;
+    }
+
+    const currentVal = preorder.shift()!;
+    const currentIndex = inorder.indexOf(currentVal);
+    const node = new TreeNode(currentVal);
+
+    node.left = recursive(preorder, inorder.slice(0, currentIndex));
+    node.right = recursive(preorder, inorder.slice(currentIndex + 1));
+
+    return node;
+  };
+
+  return recursive(preorder, inorder);
+}
+
+// test
+const levelOrder = (root: TreeNode | null): Array<number | null> => {
+  const res: Array<number | null> = [];
+  const queue: TreeNode[] = [];
+
+  if (!root) {
+    return res;
+  }
+
+  queue.push(root);
+  while (queue.length) {
+    const size = queue.length;
+    for (let i = 0; i < size; i++) {
+      const current = queue.shift()!;
+      res.push(current.val);
+      if (current.left) {
+        queue.push(current.left);
+      }
+
+      if (current.right) {
+        queue.push(current.right);
+      }
+    }
+  }
+
+  return res;
+};
+const preorder = [3, 9, 20, 15, 7];
+const inorder = [9, 3, 15, 20, 7];
+const res = buildTree(preorder, inorder);
+const printTree = levelOrder(res);
+console.log(printTree);
---
+++ Data Structure & Algorithm/Template/Binary Tree/Tree Restore/Serialize and Deserialize Binary Tree/Serialize and Deserialize Binary Tree.ts
@@ -0,0 +1,136 @@
+// 297. Serialize and Deserialize Binary Tree
+
+// Serialization is the process of converting a data structure or object into a sequence of bits so that it can be stored in a file or memory buffer, or transmitted across a network connection link to be reconstructed later in the same or another computer environment.
+
+// Design an algorithm to serialize and deserialize a binary tree. There is no restriction on how your serialization/deserialization algorithm should work. You just need to ensure that a binary tree can be serialized to a string and this string can be deserialized to the original tree structure.
+
+// Clarification: The input/output format is the same as how LeetCode serializes a binary tree. You do not necessarily need to follow this format, so please be creative and come up with different approaches yourself.
+
+// Example 1:
+// Input: root = [1,2,3,null,null,4,5]
+// Output: [1,2,3,null,null,4,5]
+
+// Example 2:
+// Input: root = []
+// Output: []
+
+// Definition for a binary tree node.
+class TreeNode {
+  val: number;
+  left: TreeNode | null;
+  right: TreeNode | null;
+  constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.left = left === undefined ? null : left;
+    this.right = right === undefined ? null : right;
+  }
+}
+
+/**
+ * Your functions will be called as such:
+ * deserialize(serialize(root));
+ */
+
+// DFS
+// use # to represent null
+export function serialize_DFS(root: TreeNode | null): string {
+  const path: Array<TreeNode | null> = [];
+  const recursive = (node: TreeNode | null) => {
+    path.push(node);
+    if (!node) {
+      return;
+    }
+    recursive(node.left);
+    recursive(node.right);
+  };
+
+  recursive(root);
+
+  return path.map((item) => (item === null ? '#' : item.val)).join(',');
+}
+
+export function deserialize_DFS(data: string): TreeNode | null {
+  const array_data: Array<TreeNode | null> = data
+    .split(',')
+    .map((item) => (item === '#' ? null : new TreeNode(Number(item))));
+
+  const recursive = (path: Array<TreeNode | null>) => {
+    if (!path.length) {
+      return null;
+    }
+
+    const current = path.shift()!;
+
+    if (!current) {
+      return null;
+    }
+
+    current.left = recursive(path);
+    current.right = recursive(path);
+
+    return current;
+  };
+
+  return recursive(array_data);
+}
+
+// BFS
+// use # to represent null
+export function serialize_BFS(root: TreeNode | null): string {
+  const path: Array<TreeNode | null> = [];
+  const queue: Array<TreeNode | null> = [];
+
+  if (!root) {
+    queue.push(null);
+  }
+
+  queue.push(root);
+
+  while (queue.length) {
+    const size = queue.length;
+    for (let i = 0; i < size; i++) {
+      const current = queue.shift()!;
+      path.push(current);
+      if (!current) {
+        break;
+      }
+      queue.push(current.left);
+      queue.push(current.right);
+    }
+  }
+
+  return path.map((item) => (item === null ? '#' : item.val)).join(',');
+}
+
+export function deserialize_BFS(data: string): TreeNode | null {
+  const array_data: Array<TreeNode | null> = data
+    .split(',')
+    .map((item) => (item === '#' ? null : new TreeNode(Number(item))));
+
+  const root: TreeNode = array_data.shift()!;
+  const queue: TreeNode[] = [];
+
+  if (!root) {
+    return null;
+  }
+
+  queue.push(root);
+
+  while (queue.length) {
+    const current = queue.shift()!;
+
+    if (array_data.length > 0) {
+      const next = array_data.shift()!;
+      current.left = next;
+    }
+    if (array_data.length > 0) {
+      const next = array_data.shift()!;
+      current.right = next;
+    }
+
+    current.left && queue.push(current.left);
+    current.right && queue.push(current.right);
+  }
+
+  return root;
+}
