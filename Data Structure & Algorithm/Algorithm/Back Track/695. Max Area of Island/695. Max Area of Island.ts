e3e70c6df56086a5caab1f5e71ad98aa49d5d474
harryxiong24 <harryxiong24@gmail.com>
2023-08-23 16:11:45 +0800

    2023.8.23

---
+++ Data Structure & Algorithm/Algorithm/Back Track/695. Max Area of Island/695. Max Area of Island.ts
@@ -0,0 +1,68 @@
+// 695. Max Area of Island
+
+// You are given an m x n binary matrix grid. An island is a group of 1's (representing land) connected 4-directionally (horizontal or vertical.) You may assume all four edges of the grid are surrounded by water.
+
+// The area of an island is the number of cells with a value 1 in the island.
+
+// Return the maximum area of an island in grid. If there is no island, return 0.
+
+// Example 1:
+// Input: grid = [[0,0,1,0,0,0,0,1,0,0,0,0,0],[0,0,0,0,0,0,0,1,1,1,0,0,0],[0,1,1,0,1,0,0,0,0,0,0,0,0],[0,1,0,0,1,1,0,0,1,0,1,0,0],[0,1,0,0,1,1,0,0,1,1,1,0,0],[0,0,0,0,0,0,0,0,0,0,1,0,0],[0,0,0,0,0,0,0,1,1,1,0,0,0],[0,0,0,0,0,0,0,1,1,0,0,0,0]]
+// Output: 6
+// Explanation: The answer is not 11, because the island must be connected 4-directionally.
+
+// Example 2:
+// Input: grid = [[0,0,0,0,0,0,0,0]]
+// Output: 0
+
+export function maxAreaOfIsland(grid: number[][]): number {
+  let maxArea: number = 0;
+  const directions = [
+    [1, 0],
+    [-1, 0],
+    [0, 1],
+    [0, -1],
+  ];
+  const visited = new Set();
+
+  const recursive = (i: number, j: number, count: string[]) => {
+    if (i < 0 || i >= grid.length || j < 0 || j >= grid[0].length) {
+      return;
+    }
+    if (grid[i][j] === 0 || visited.has(`row${i}-col${j}`)) {
+      return;
+    }
+    visited.add(`row${i}-col${j}`);
+    count.push(`row${i}-col${j}`);
+    for (const item of directions) {
+      recursive(i + item[0], j + item[1], count);
+    }
+  };
+
+  for (let i = 0; i < grid.length; i++) {
+    for (let j = 0; j < grid[0].length; j++) {
+      if (grid[i][j] === 1 && !visited.has(`row${i}-col${j}`)) {
+        let area: string[] = [];
+        recursive(i, j, area);
+        if (area.length > maxArea) {
+          maxArea = area.length;
+        }
+      }
+    }
+  }
+
+  return maxArea;
+}
+
+// test
+const res = maxAreaOfIsland([
+  [0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0],
+  [0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 0, 0],
+  [0, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0],
+]);
+console.log(res);
