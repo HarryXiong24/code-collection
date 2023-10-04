285c409f6340fc06967f8429637f03fc4a95265f
harryxiong24 <harryxiong24@gmail.com>
2023-10-04 16:59:29 -0700

    2023.10.4

--- Data Structure & Algorithm/Algorithm/Back Track/22. Generate Parentheses/22. Generate Parentheses.py
+++ Data Structure & Algorithm/Algorithm/Back Track/22. Generate Parentheses/22. Generate Parentheses.py
@@ -15,4 +15,28 @@ from typing import List
 
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
-        
+        res: List[str] = []
+
+        def backtrack(s: List[str], left: int, right: int):
+            if len(s) == 2 * n:
+                res.append(''.join(s))
+                return
+
+            if left < n:
+                s.append('(')
+                backtrack(s, left + 1, right)
+                s.pop()
+
+            if right < left:
+                s.append(')')
+                backtrack(s, left, right + 1)
+                s.pop()
+
+        backtrack([], 0, 0)
+        return res
+
+
+# test
+solution = Solution()
+res = solution.generateParenthesis(3)
+print(res)
