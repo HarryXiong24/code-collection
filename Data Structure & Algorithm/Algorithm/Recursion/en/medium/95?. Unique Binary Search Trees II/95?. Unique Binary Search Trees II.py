883bc49ab7e5133243b834cd73bf9d4226aebbd5
harryxiong24 <harryxiong24@gmail.com>
2023-10-11 21:39:43 -0700

    2023.10.11

---
+++ Data Structure & Algorithm/Algorithm/Recursion/en/medium/95?. Unique Binary Search Trees II/95?. Unique Binary Search Trees II.py
@@ -0,0 +1,45 @@
+# 95. Unique Binary Search Trees II
+
+# Given an integer n, return all the structurally unique BST's (binary search trees), which has exactly n nodes of unique values from 1 to n. Return the answer in any order.
+
+# Example 1:
+# Input: n = 3
+# Output: [[1,null,2,null,3],[1,null,3,2],[2,1,3],[3,1,null,null,2],[3,2,null,1]]
+
+# Example 2:
+# Input: n = 1
+# Output: [[1]]
+
+# Definition for a binary tree node.
+from typing import List, Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def generateTrees(self, n: int) -> List[Optional[TreeNode]]:
+        dp = [[] for _ in range(n + 1)]
+        dp[0].append(None)
+
+        for numberOfNodes in range(1, n + 1):
+            for i in range(1, numberOfNodes + 1):
+                j = numberOfNodes - i
+                for left in dp[i - 1]:
+                    for right in dp[j]:
+                        root = TreeNode(i, left, self.clone(right, i))
+                        dp[numberOfNodes].append(root)
+
+        return dp[n]
+
+    def clone(self, node: Optional[TreeNode], offset: int) -> Optional[TreeNode]:
+        if not node:
+            return None
+        cloned_node = TreeNode(node.val + offset)
+        cloned_node.left = self.clone(node.left, offset)
+        cloned_node.right = self.clone(node.right, offset)
+        return cloned_node
