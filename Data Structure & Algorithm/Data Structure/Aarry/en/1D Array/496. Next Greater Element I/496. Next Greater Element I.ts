1918234f1f83d51d3e0ce322871040aad4b58681
harryxiong24 <harryxiong24@gmail.com>
2023-10-20 14:41:38 -0700

    2023.10.20

---
+++ Data Structure & Algorithm/Data Structure/Aarry/en/1D Array/496. Next Greater Element I/496. Next Greater Element I.ts
@@ -0,0 +1,47 @@
+// 496. Next Greater Element I
+
+// The next greater element of some element x in an array is the first greater element that is to the right of x in the same array.
+
+// You are given two distinct 0-indexed integer arrays nums1 and nums2, where nums1 is a subset of nums2.
+
+// For each 0 <= i < nums1.length, find the index j such that nums1[i] == nums2[j] and determine the next greater element of nums2[j] in nums2. If there is no next greater element, then the answer for this query is -1.
+
+// Return an array ans of length nums1.length such that ans[i] is the next greater element as described above.
+
+// Example 1:
+// Input: nums1 = [4,1,2], nums2 = [1,3,4,2]
+// Output: [-1,3,-1]
+// Explanation: The next greater element for each value of nums1 is as follows:
+// - 4 is underlined in nums2 = [1,3,4,2]. There is no next greater element, so the answer is -1.
+// - 1 is underlined in nums2 = [1,3,4,2]. The next greater element is 3.
+// - 2 is underlined in nums2 = [1,3,4,2]. There is no next greater element, so the answer is -1.
+
+// Example 2:
+// Input: nums1 = [2,4], nums2 = [1,2,3,4]
+// Output: [3,-1]
+// Explanation: The next greater element for each value of nums1 is as follows:
+// - 2 is underlined in nums2 = [1,2,3,4]. The next greater element is 3.
+// - 4 is underlined in nums2 = [1,2,3,4]. There is no next greater element, so the answer is -1.
+
+export function nextGreaterElement(nums1: number[], nums2: number[]): number[] {
+  const res: number[] = [];
+
+  for (let i = 0; i < nums1.length; i++) {
+    const start = nums2.indexOf(nums1[i]);
+    let isExist = false;
+    for (let j = start + 1; j < nums2.length; j++) {
+      if (nums2[j] > nums2[start]) {
+        res.push(nums2[j]);
+        isExist = true;
+        break;
+      }
+    }
+    !isExist && res.push(-1);
+  }
+
+  return res;
+}
+
+// test
+const res = nextGreaterElement([4, 1, 2], [1, 3, 4, 2]);
+console.log(res);
