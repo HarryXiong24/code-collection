71c465d0926e0433c0820991d3cdfc25dd3783ea
harryxiong24 <harryxiong24@gmail.com>
2023-10-31 13:00:28 -0700

    2023.10.31

--- Data Structure & Algorithm/Algorithm/Recursion/en/medium/148. Sort List/148. Sort List.ts
+++ Data Structure & Algorithm/Algorithm/Recursion/en/medium/148. Sort List/148. Sort List.ts
@@ -1,4 +1,4 @@
-// 92. Reverse Linked List II
+// 148. Sort List
 
 // Given the head of a singly linked list and two integers left and right where left <= right, reverse the nodes of the list from position left to position right, and return the reversed list.
 
