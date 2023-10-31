fc95add907573f0c40ab22c75dc279925c99d332
harryxiong24 <harryxiong24@gmail.com>
2023-10-31 16:37:11 -0700

    2023.10.31

---
+++ Problem Collection/Super Easy/1290. Convert Binary Number in a Linked List to Integer/1290. Convert Binary Number in a Linked List to Integer.ts
@@ -0,0 +1,38 @@
+// 1290. Convert Binary Number in a Linked List to Integer
+
+// Given head which is a reference node to a singly-linked list. The value of each node in the linked list is either 0 or 1. The linked list holds the binary representation of a number.
+
+// Return the decimal value of the number in the linked list.
+
+// The most significant bit is at the head of the linked list.
+
+// Example 1:
+// Input: head = [1,0,1]
+// Output: 5
+// Explanation: (101) in base 2 = (5) in base 10
+
+// Example 2:
+// Input: head = [0]
+// Output: 0
+
+//  Definition for singly-linked list.
+class ListNode {
+  val: number;
+  next: ListNode | null;
+  constructor(val?: number, next?: ListNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.next = next === undefined ? null : next;
+  }
+}
+
+export function getDecimalValue(head: ListNode | null): number {
+  let node = head;
+  let s: string = '';
+
+  while (node !== null) {
+    s = s + node.val;
+    node = node.next;
+  }
+
+  return parseInt(s, 2);
+}
