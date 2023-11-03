0d4ecbde9599194c080b4fb38dce84a64501e45a
harryxiong24 <harryxiong24@gmail.com>
2023-11-03 13:43:52 -0700

    2023.11.3

---
+++ Data Structure & Algorithm/Algorithm/Recursion/en/medium/814. Binary Tree Pruning/814. Binary Tree Pruning.py
@@ -0,0 +1,54 @@
+# 814. Binary Tree Pruning
+
+# iven the root of a binary tree, return the same tree where every subtree (of the given tree) not containing a 1 has been removed.
+
+# A subtree of a node node is node plus every node that is a descendant of node.
+
+# Example 1:
+# Input: root = [1,null,0,0,1]
+# Output: [1,null,0,null,1]
+# Explanation: 
+# Only the red nodes satisfy the property "every subtree not containing a 1".
+# The diagram on the right represents the answer.
+
+# Example 2:
+# Input: root = [1,0,1,0,0,0,1]
+# Output: [1,null,1,null,1]
+
+# Example 3:
+# Input: root = [1,1,0,1,1,0,1,0]
+# Output: [1,1,0,1,1,null,1]
+
+from typing import Optional
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def pruneTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        def recursive(node: Optional[TreeNode]) -> bool:
+            if not node:
+                return False;
+      
+            left = recursive(node.left);
+            right = recursive(node.right);
+            
+            if not left:
+                node.left = None;
+            if not right:
+                node.right = None;
+                
+            return node.val == 1 or left or right;
+          
+        if not recursive(root):
+            return None;
+        
+        return root;
+
+          
+          
