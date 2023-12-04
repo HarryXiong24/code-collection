51d0a8c41f1bb14c9cc6fd2303c1473c381a63ce
harryxiong24 <harryxiong24@gmail.com>
2023-12-03 22:46:12 -0800

    2023.12.3

--- Data Structure & Algorithm/Algorithm/Dynamic Programming/ medium/322. Coin Change/322. Coin Change.py
+++ Data Structure & Algorithm/Algorithm/Dynamic Programming/ medium/322. Coin Change/322. Coin Change.py
@@ -29,8 +29,8 @@ class Solution:
         dp[0] = 0
 
         for coin in coins:
-            for x in range(coin, amount + 1):
-                dp[x] = min(dp[x], dp[x - coin] + 1)
+            for i in range(coin, amount + 1):
+                dp[i] = min(dp[i], dp[i - coin] + 1)
 
         if dp[amount] != float("inf"):
             return dp[amount]
---
+++ Data Structure & Algorithm/Algorithm/Dynamic Programming/ medium/518. Coin Change II/518. Coin Change II.py
@@ -0,0 +1,52 @@
+# 518. Coin Change II
+
+# You are given an integer array coins representing coins of different denominations and an integer amount representing a total amount of money.
+
+# Return the number of combinations that make up that amount. If that amount of money cannot be made up by any combination of the coins, return 0.
+
+# You may assume that you have an infinite number of each kind of coin.
+
+# The answer is guaranteed to fit into a signed 32-bit integer.
+
+# Example 1:
+# Input: amount = 5, coins = [1,2,5]
+# Output: 4
+# Explanation: there are four ways to make up the amount:
+# 5=5
+# 5=2+2+1
+# 5=2+1+1+1
+# 5=1+1+1+1+1
+
+# Example 2:
+# Input: amount = 3, coins = [2]
+# Output: 0
+# Explanation: the amount of 3 cannot be made up just with coins of 2.
+
+# Example 3:
+# Input: amount = 10, coins = [10]
+# Output: 1
+
+from typing import List
+
+
+class Solution:
+    def change(self, amount: int, coins: List[int]) -> int:
+        dp = [[0] * (amount + 1) for _ in range(len(coins) + 1)]
+
+        for i in range(len(coins)):
+            dp[i][0] = 1
+
+        for i in range(len(coins) - 1, -1, -1):
+            for j in range(1, amount + 1):
+                if coins[i] > j:
+                    dp[i][j] = dp[i + 1][j]
+                else:
+                    dp[i][j] = dp[i + 1][j] + dp[i][j - coins[i]]
+
+        return dp[0][amount]
+
+
+# test
+solution = Solution()
+res = solution.change(5, [1, 2, 5])
+print(res)
