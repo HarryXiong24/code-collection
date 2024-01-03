c78f0dfc441d5c395c40d5a3f2e87b357a72f1e4
harryxiong24 <harryxiong24@gmail.com>
2024-01-03 11:34:58 -0800

    2024.1.3

---
+++ SQL/Review/182. Duplicate Emails/182. Duplicate Emails.sql
@@ -0,0 +1,42 @@
+-- 182. Duplicate Emails
+
+-- Table: Person
+
+-- +-------------+---------+
+-- | Column Name | Type    |
+-- +-------------+---------+
+-- | id          | int     |
+-- | email       | varchar |
+-- +-------------+---------+
+-- id is the primary key (column with unique values) for this table.
+-- Each row of this table contains an email. The emails will not contain uppercase letters.
+ 
+
+-- Write a solution to report all the duplicate emails. Note that it's guaranteed that the email field is not NULL.
+
+-- Return the result table in any order.
+
+-- The result format is in the following example.
+
+-- Example 1:
+-- Input: 
+-- Person table:
+-- +----+---------+
+-- | id | email   |
+-- +----+---------+
+-- | 1  | a@b.com |
+-- | 2  | c@d.com |
+-- | 3  | a@b.com |
+-- +----+---------+
+-- Output: 
+-- +---------+
+-- | Email   |
+-- +---------+
+-- | a@b.com |
+-- +---------+
+-- Explanation: a@b.com is repeated two times.
+
+select email as Email
+from Person
+group by email
+having count(email) >= 2
