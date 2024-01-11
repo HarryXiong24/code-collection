6ed66dc5ac8764803945a3e64ba98b300bbddcda
harryxiong24 <harryxiong24@gmail.com>
2024-01-11 10:18:07 -0800

    2024.1.11

---
+++ Data Structure & Algorithm/Algorithm/Dynamic Programming/Easy/1137. N-th Tribonacci Number/1137. N-th Tribonacci Number.ts
@@ -0,0 +1,36 @@
+// 1137. N-th Tribonacci Number
+
+// The Tribonacci sequence Tn is defined as follows:
+
+// T0 = 0, T1 = 1, T2 = 1, and Tn+3 = Tn + Tn+1 + Tn+2 for n >= 0.
+
+// Given n, return the value of Tn.
+
+// Example 1:
+// Input: n = 4
+// Output: 4
+// Explanation:
+// T_3 = 0 + 1 + 1 = 2
+// T_4 = 1 + 1 + 2 = 4
+
+// Example 2:
+// Input: n = 25
+// Output: 1389537
+
+export function tribonacci(n: number): number {
+  const dp: number[] = [];
+
+  dp[0] = 0;
+  dp[1] = 1;
+  dp[2] = 1;
+
+  for (let i = 3; i <= n; i++) {
+    dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
+  }
+
+  return dp[n];
+}
+
+// test
+const res = tribonacci(4);
+console.log(res);
