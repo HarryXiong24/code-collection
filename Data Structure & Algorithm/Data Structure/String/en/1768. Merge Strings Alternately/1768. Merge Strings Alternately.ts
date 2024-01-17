86463df19621ffb3a86a3eeef6f9e8c354e8c12f
harryxiong24 <harryxiong24@gmail.com>
2024-01-17 09:40:29 -0800

    2024.1.17

---
+++ Data Structure & Algorithm/Data Structure/String/en/1768. Merge Strings Alternately/1768. Merge Strings Alternately.ts
@@ -0,0 +1,48 @@
+// 1768. Merge Strings Alternately
+
+// You are given two strings word1 and word2. Merge the strings by adding letters in alternating order, starting with word1. If a string is longer than the other, append the additional letters onto the end of the merged string.
+
+// Return the merged string.
+
+// Example 1:
+// Input: word1 = "abc", word2 = "pqr"
+// Output: "apbqcr"
+// Explanation: The merged string will be merged as so:
+// word1:  a   b   c
+// word2:    p   q   r
+// merged: a p b q c r
+
+// Example 2:
+// Input: word1 = "ab", word2 = "pqrs"
+// Output: "apbqrs"
+// Explanation: Notice that as word2 is longer, "rs" is appended to the end.
+// word1:  a   b
+// word2:    p   q   r   s
+// merged: a p b q   r   s
+
+// Example 3:
+// Input: word1 = "abcd", word2 = "pq"
+// Output: "apbqcd"
+// Explanation: Notice that as word1 is longer, "cd" is appended to the end.
+// word1:  a   b   c   d
+// word2:    p   q
+// merged: a p b q c   d
+
+export function mergeAlternately(word1: string, word2: string): string {
+  let res = '';
+
+  for (let i = 0; i < Math.max(word1.length, word2.length); i++) {
+    if (i < word1.length) {
+      res += word1[i];
+    }
+    if (i < word2.length) {
+      res += word2[i];
+    }
+  }
+
+  return res;
+}
+
+// test
+const res = mergeAlternately('abcd', 'pq');
+console.log(res);
