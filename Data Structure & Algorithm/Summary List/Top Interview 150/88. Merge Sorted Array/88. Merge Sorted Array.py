adceef0d7ef43eae44e62078d3112bbb33a50e70
harryxiong24 <harryxiong24@gmail.com>
2024-02-02 23:39:16 -0800

    2024.2.1

---
+++ Data Structure & Algorithm/Summary List/Top Interview 150/88. Merge Sorted Array/88. Merge Sorted Array.py
@@ -0,0 +1,60 @@
+# 88. Merge Sorted Array
+
+# You are given two integer arrays nums1 and nums2, sorted in non-decreasing order, and two integers m and n, representing the number of elements in nums1 and nums2 respectively.
+
+# Merge nums1 and nums2 into a single array sorted in non-decreasing order.
+
+# The final sorted array should not be returned by the function, but instead be stored inside the array nums1. To accommodate this, nums1 has a length of m + n, where the first m elements denote the elements that should be merged, and the last n elements are set to 0 and should be ignored. nums2 has a length of n.
+
+# Example 1:
+# Input: nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
+# Output: [1,2,2,3,5,6]
+# Explanation: The arrays we are merging are [1,2,3] and [2,5,6].
+# The result of the merge is [1,2,2,3,5,6] with the underlined elements coming from nums1.
+
+# Example 2:
+# Input: nums1 = [1], m = 1, nums2 = [], n = 0
+# Output: [1]
+# Explanation: The arrays we are merging are [1] and [].
+# The result of the merge is [1].
+
+# Example 3:
+# Input: nums1 = [0], m = 0, nums2 = [1], n = 1
+# Output: [1]
+# Explanation: The arrays we are merging are [] and [1].
+# The result of the merge is [1].
+# Note that because m = 0, there are no elements in nums1. The 0 is only there to ensure the merge result can fit in nums1.
+
+
+from typing import List
+
+
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+
+        # Set p1 and p2 to point to the end of their respective arrays.
+        p1 = m - 1
+        p2 = n - 1
+
+        # And move p backward through the array, each time writing
+        # the smallest value pointed at by p1 or p2.
+        for p in range(n + m - 1, -1, -1):
+            if p2 < 0:
+                break
+            if p1 >= 0 and nums1[p1] > nums2[p2]:
+                nums1[p] = nums1[p1]
+                p1 -= 1
+            else:
+                nums1[p] = nums2[p2]
+                p2 -= 1
+
+
+# test
+num1 = [1, 2, 3, 0, 0, 0]
+num2 = [2, 5, 6]
+solution = Solution()
+res = solution.merge(num1, 3, num2, 3)
+print(num1)
---
+++ Data Structure & Algorithm/Summary List/Top Interview 150/88. Merge Sorted Array/88. Merge Sorted Array.ts
@@ -0,0 +1,57 @@
+// 88. Merge Sorted Array
+
+// You are given two integer arrays nums1 and nums2, sorted in non-decreasing order, and two integers m and n, representing the number of elements in nums1 and nums2 respectively.
+
+// Merge nums1 and nums2 into a single array sorted in non-decreasing order.
+
+// The final sorted array should not be returned by the function, but instead be stored inside the array nums1. To accommodate this, nums1 has a length of m + n, where the first m elements denote the elements that should be merged, and the last n elements are set to 0 and should be ignored. nums2 has a length of n.
+
+// Example 1:
+// Input: nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
+// Output: [1,2,2,3,5,6]
+// Explanation: The arrays we are merging are [1,2,3] and [2,5,6].
+// The result of the merge is [1,2,2,3,5,6] with the underlined elements coming from nums1.
+
+// Example 2:
+// Input: nums1 = [1], m = 1, nums2 = [], n = 0
+// Output: [1]
+// Explanation: The arrays we are merging are [1] and [].
+// The result of the merge is [1].
+
+// Example 3:
+// Input: nums1 = [0], m = 0, nums2 = [1], n = 1
+// Output: [1]
+// Explanation: The arrays we are merging are [] and [1].
+// The result of the merge is [1].
+// Note that because m = 0, there are no elements in nums1. The 0 is only there to ensure the merge result can fit in nums1.
+
+/**
+ Do not return anything, modify nums1 in-place instead.
+ */
+// O(m+n)
+export function merge(nums1: number[], m: number, nums2: number[], n: number): void {
+  let p1 = m - 1;
+  let p2 = n - 1;
+
+  let total = n + m - 1;
+  while (total >= 0) {
+    if (p2 < 0) {
+      break;
+    }
+    if (p1 >= 0 && nums1[p1] >= nums2[p2]) {
+      nums1[total] = nums1[p1];
+      total--;
+      p1--;
+    } else {
+      nums1[total] = nums2[p2];
+      total--;
+      p2--;
+    }
+  }
+}
+
+// test
+const num1 = [1, 2, 3, 0, 0, 0];
+const num2 = [2, 5, 6];
+merge(num1, 3, num2, 3);
+console.log(num1);
