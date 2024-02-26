03047b62215a19c5936437ba86d9ea57d853e6a6
harryxiong24 <harryxiong24@gmail.com>
2024-02-26 15:13:19 -0800

    2024.2.26

---
+++ Tests/Review1/heap-sort/tempCodeRunnerFile.ts
@@ -0,0 +1,6 @@
+  for (let i = nums.length - 1; i >= 0; i--) {
+  //     const temp = nums[0];
+  //     nums[0] = nums[nums.length - 1];
+  //     nums[nums.length - 1] = temp;
+  //     heapify(nums, i, 0);
+  //   }
