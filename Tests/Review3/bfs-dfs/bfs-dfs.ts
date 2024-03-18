2e4b1e707b70667a40964fe1a52929c029f4c7f7
harryxiong24 <harryxiong24@gmail.com>
2024-03-18 10:11:45 -0700

    2024.3.18

---
+++ Tests/Review3/bfs-dfs/bfs-dfs.ts
@@ -0,0 +1,108 @@
+interface TestNode {
+  value: any;
+  neighbors: TestNode[];
+}
+
+export function BFS(root: TestNode): number {
+  if (!root) {
+    return -1;
+  }
+
+  const queue = [root];
+  let layer = 0;
+
+  while (queue.length) {
+    const size = queue.length;
+    for (let i = 0; i < size; i++) {
+      const current = queue.shift()!;
+      console.log(current.value);
+      for (const item of current.neighbors) {
+        queue.push(item);
+      }
+    }
+    layer += 1;
+  }
+
+  return layer;
+}
+
+export function DFS_Iteration(root: TestNode) {
+  if (!root) {
+    return -1;
+  }
+
+  const stack = [root];
+
+  while (stack.length) {
+    const current = stack.pop()!;
+    console.log(current.value);
+
+    for (const item of current.neighbors) {
+      stack.push(item);
+    }
+  }
+}
+
+export function DFS_Recursion(root: TestNode) {
+  if (!root) {
+    return -1;
+  }
+
+  const recursion = (node: TestNode) => {
+    if (!node) {
+      return;
+    }
+    console.log(node.value);
+    for (const item of node.neighbors) {
+      recursion(item);
+    }
+  };
+
+  recursion(root);
+}
+
+// test
+const root: TestNode = {
+  value: 1,
+  neighbors: [
+    {
+      value: 2,
+      neighbors: [
+        {
+          value: 4,
+          neighbors: [],
+        },
+        {
+          value: 5,
+          neighbors: [],
+        },
+      ],
+    },
+    {
+      value: 3,
+      neighbors: [
+        {
+          value: 6,
+          neighbors: [
+            {
+              value: 7,
+              neighbors: [],
+            },
+          ],
+        },
+      ],
+    },
+  ],
+};
+
+// BFS
+const res = BFS(root);
+console.log('layer', res);
+
+// DFS Recursion
+console.log('------');
+DFS_Recursion(root);
+
+// DFS Iteration
+console.log('------');
+DFS_Iteration(root);
