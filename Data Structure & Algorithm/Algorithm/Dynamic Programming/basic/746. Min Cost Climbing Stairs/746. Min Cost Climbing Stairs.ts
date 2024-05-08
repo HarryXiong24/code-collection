630fcabc55bb1af8343158e8b90851e25c1c8815
harryxiong24 <harryxiong24@gmail.com>
2024-05-07 17:11:02 -0700

    2024.5.7

---
+++ Data Structure & Algorithm/Algorithm/Dynamic Programming/basic/746. Min Cost Climbing Stairs/746. Min Cost Climbing Stairs.ts
@@ -0,0 +1,44 @@
+// 746. Min Cost Climbing Stairs
+
+// You are given an integer array cost where cost[i] is the cost of ith step on a staircase. Once you pay the cost, you can either climb one or two steps.
+
+// You can either start from the step with index 0, or the step with index 1.
+
+// Return the minimum cost to reach the top of the floor.
+
+// Example 1:
+// Input: cost = [10,15,20]
+// Output: 15
+// Explanation: You will start at index 1.
+// - Pay 15 and climb two steps to reach the top.
+// The total cost is 15.
+
+// Example 2:
+// Input: cost = [1,100,1,1,1,100,1,1,100,1]
+// Output: 6
+// Explanation: You will start at index 0.
+// - Pay 1 and climb two steps to reach index 2.
+// - Pay 1 and climb two steps to reach index 4.
+// - Pay 1 and climb two steps to reach index 6.
+// - Pay 1 and climb one step to reach index 7.
+// - Pay 1 and climb two steps to reach index 9.
+// - Pay 1 and climb one step to reach the top.
+// The total cost is 6.
+
+export function minCostClimbingStairs(cost: number[]): number {
+  const dp: number[] = new Array(cost.length + 1).fill(0);
+
+  dp[0] = 0;
+  dp[1] = 0;
+  dp[2] = Math.min(cost[0], cost[1]);
+
+  for (let i = 3; i <= cost.length; i++) {
+    dp[i] = Math.min(dp[i - 1] + cost[i - 1], dp[i - 2] + cost[i - 2]);
+  }
+
+  return dp[cost.length];
+}
+
+// test
+const res = minCostClimbingStairs([1, 100, 1, 1, 1, 100, 1, 1, 100, 1]);
+console.log(res);
