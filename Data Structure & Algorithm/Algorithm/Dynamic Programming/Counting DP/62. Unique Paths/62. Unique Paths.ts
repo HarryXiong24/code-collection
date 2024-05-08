c48922f91d193e5ed792b5dd69601796b057d76d
harryxiong24 <harryxiong24@gmail.com>
2024-05-07 17:28:27 -0700

    2024.5.7

---
+++ Data Structure & Algorithm/Algorithm/Dynamic Programming/Counting DP/62. Unique Paths/62. Unique Paths.ts
@@ -0,0 +1,44 @@
+// 62. Unique Paths
+
+// There is a robot on an m x n grid. The robot is initially located at the top-left corner (i.e., grid[0][0]). The robot tries to move to the bottom-right corner (i.e., grid[m - 1][n - 1]). The robot can only move either down or right at any point in time.
+
+// Given the two integers m and n, return the number of possible unique paths that the robot can take to reach the bottom-right corner.
+
+// The test cases are generated so that the answer will be less than or equal to 2 * 109.
+
+// Example 1:
+// Input: m = 3, n = 7
+// Output: 28
+
+// Example 2:
+// Input: m = 3, n = 2
+// Output: 3
+// Explanation: From the top-left corner, there are a total of 3 ways to reach the bottom-right corner:
+// 1. Right -> Down -> Down
+// 2. Down -> Down -> Right
+// 3. Down -> Right -> Down
+
+export function uniquePaths(m: number, n: number): number {
+  const dp: number[][] = [...new Array(m)].map(() => new Array(n).fill(0));
+
+  // init
+  for (let i = 0; i < m; i++) {
+    for (let j = 0; j < n; j++) {
+      if (i === 0 || j === 0) {
+        dp[i][j] = 1;
+      }
+    }
+  }
+
+  for (let i = 1; i < m; i++) {
+    for (let j = 1; j < n; j++) {
+      dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+    }
+  }
+
+  return dp[m - 1][n - 1];
+}
+
+// test
+const res = uniquePaths(3, 7);
+console.log(res);
