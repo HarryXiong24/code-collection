ab7ac43dddd65df2becdc57f14fdad83bfb24389
harryxiong24 <harryxiong24@gmail.com>
2024-06-19 11:03:11 -0700

    2024.6.19

---
+++ Data Structure & Algorithm/Summary List/Code Random Thoughts/Array/@189. Rotate Array/189. Rotate Array.ts
@@ -0,0 +1,44 @@
+// 189. Rotate Array
+
+// Given an integer array nums, rotate the array to the right by k steps, where k is non-negative.
+
+// Example 1:
+// Input: nums = [1,2,3,4,5,6,7], k = 3
+// Output: [5,6,7,1,2,3,4]
+// Explanation:
+// rotate 1 steps to the right: [7,1,2,3,4,5,6]
+// rotate 2 steps to the right: [6,7,1,2,3,4,5]
+// rotate 3 steps to the right: [5,6,7,1,2,3,4]
+
+// Example 2:
+// Input: nums = [-1,-100,3,99], k = 2
+// Output: [3,99,-1,-100]
+// Explanation:
+// rotate 1 steps to the right: [99,-1,-100,3]
+// rotate 2 steps to the right: [3,99,-1,-100]
+
+/**
+ Do not return anything, modify nums in-place instead.
+ */
+export function rotate(nums: number[], k: number): void {
+  k = k % nums.length;
+
+  const reverseByRange = (nums: number[], left: number, right: number): void => {
+    while (left < right) {
+      const temp = nums[left];
+      nums[left] = nums[right];
+      nums[right] = temp;
+      left++;
+      right--;
+    }
+  };
+
+  reverseByRange(nums, 0, nums.length - k - 1);
+  reverseByRange(nums, nums.length - k, nums.length - 1);
+  reverseByRange(nums, 0, nums.length - 1);
+}
+
+// test
+const arr = [1, 2, 3, 4, 5, 6, 7];
+rotate(arr, 3);
+console.log(arr);
