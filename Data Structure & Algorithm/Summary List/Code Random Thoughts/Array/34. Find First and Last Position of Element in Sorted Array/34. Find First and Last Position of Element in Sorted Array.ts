88f3e0bea88fa4c9f270060fdfa96a085e45b425
harryxiong24 <harryxiong24@gmail.com>
2024-06-19 11:46:22 -0700

    2024.6.19

---
+++ Data Structure & Algorithm/Summary List/Code Random Thoughts/Array/34. Find First and Last Position of Element in Sorted Array/34. Find First and Last Position of Element in Sorted Array.ts
@@ -0,0 +1,47 @@
+// 34. Find First and Last Position of Element in Sorted Array
+
+// Given an array of integers nums sorted in non-decreasing order, find the starting and ending position of a given target value.
+
+// If target is not found in the array, return [-1, -1].
+
+// You must write an algorithm with O(log n) runtime complexity.
+
+// Example 1:
+// Input: nums = [5,7,7,8,8,10], target = 8
+// Output: [3,4]
+
+// Example 2:
+// Input: nums = [5,7,7,8,8,10], target = 6
+// Output: [-1,-1]
+
+// Example 3:
+// Input: nums = [], target = 0
+// Output: [-1,-1]
+
+export function searchRange(nums: number[], target: number): number[] {
+  let left = 0;
+  let right = nums.length - 1;
+
+  while (left <= right) {
+    const mid = left + Math.floor((right - left) / 2);
+    if (target < nums[mid]) {
+      right = mid - 1;
+    } else if (target > nums[mid]) {
+      left = mid + 1;
+    } else {
+      while (nums[left] !== target) {
+        left++;
+      }
+      while (nums[right] !== target) {
+        right--;
+      }
+      return [left, right];
+    }
+  }
+
+  return [-1, -1];
+}
+
+// test
+const res = searchRange([5, 7, 7, 8, 8, 10], 8);
+console.log(res);
