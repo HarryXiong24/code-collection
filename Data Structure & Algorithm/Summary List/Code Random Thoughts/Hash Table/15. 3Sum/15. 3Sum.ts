77dc03be99d66a5d9c651f0284c7c3ce59537ec9
harryxiong24 <harryxiong24@gmail.com>
2024-06-23 11:58:56 -0700

    2024.6.23

---
+++ Data Structure & Algorithm/Summary List/Code Random Thoughts/Hash Table/15. 3Sum/15. 3Sum.ts
@@ -0,0 +1,65 @@
+// 15. 3Sum
+
+// Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
+
+// Notice that the solution set must not contain duplicate triplets.
+
+// Example 1:
+// Input: nums = [-1,0,1,2,-1,-4]
+// Output: [[-1,-1,2],[-1,0,1]]
+// Explanation:
+// nums[0] + nums[1] + nums[2] = (-1) + 0 + 1 = 0.
+// nums[1] + nums[2] + nums[4] = 0 + 1 + (-1) = 0.
+// nums[0] + nums[3] + nums[4] = (-1) + 2 + (-1) = 0.
+// The distinct triplets are [-1,0,1] and [-1,-1,2].
+// Notice that the order of the output and the order of the triplets does not matter.
+
+// Example 2:
+// Input: nums = [0,1,1]
+// Output: []
+// Explanation: The only possible triplet does not sum up to 0.
+
+// Example 3:
+// Input: nums = [0,0,0]
+// Output: [[0,0,0]]
+// Explanation: The only possible triplet sums up to 0.
+
+export function threeSum(nums: number[]): number[][] {
+  const results: number[][] = [];
+  nums.sort((a, b) => a - b);
+
+  for (let i = 0; i < nums.length; i++) {
+    if (i > 0 && nums[i] === nums[i - 1]) {
+      continue;
+    }
+
+    let left = i + 1;
+    let right = nums.length - 1;
+
+    while (left < right) {
+      if (nums[i] + nums[left] + nums[right] < 0) {
+        left++;
+      } else if (nums[i] + nums[left] + nums[right] > 0) {
+        right--;
+      } else {
+        results.push([nums[i], nums[left], nums[right]]);
+
+        while (left < right && nums[left + 1] === nums[left]) {
+          left++;
+        }
+        while (left < right && nums[right - 1] === nums[right]) {
+          right--;
+        }
+
+        left++;
+        right--;
+      }
+    }
+  }
+
+  return results;
+}
+
+// test
+const res = threeSum([-1, 0, 1, 2, -1, -4]);
+console.log(res);
