54c94bf68e786e54040bb1809dcc366a09f9e0d8
harryxiong24 <harryxiong24@gmail.com>
2024-06-23 11:13:30 -0700

    2024.6.23

---
+++ Data Structure & Algorithm/Summary List/Code Random Thoughts/Hash Table/@202. Happy Number/202. Happy Number.ts
@@ -0,0 +1,48 @@
+// 202. Happy Number
+
+// Write an algorithm to determine if a number n is happy.
+
+// A happy number is a number defined by the following process:
+
+// Starting with any positive integer, replace the number by the sum of the squares of its digits.
+// Repeat the process until the number equals 1 (where it will stay), or it loops endlessly in a cycle which does not include 1.
+// Those numbers for which this process ends in 1 are happy.
+// Return true if n is a happy number, and false if not.
+
+// Example 1:
+// Input: n = 19
+// Output: true
+// Explanation:
+// 12 + 92 = 82
+// 82 + 22 = 68
+// 62 + 82 = 100
+// 12 + 02 + 02 = 1
+
+// Example 2:
+// Input: n = 2
+// Output: false
+
+export function isHappy(n: number): boolean {
+  const set: Set<number> = new Set();
+
+  const calcSum = (val: number): number => {
+    let sum = 0;
+    while (val > 0) {
+      let temp = val % 10;
+      sum += temp * temp;
+      val = Math.floor(val / 10);
+    }
+    return sum;
+  };
+
+  while (n !== 1 && !set.has(n)) {
+    set.add(n);
+    n = calcSum(n);
+  }
+
+  return n === 1;
+}
+
+// test
+const res = isHappy(19);
+console.log(res);
