ad84ff27251a9f236a7846c328e477c433372042
harryxiong24 <harryxiong24@gmail.com>
2024-06-27 23:21:29 -0700

    2024.6.27

---
+++ Tests/Phase 3/Review1/prim/prim.ts
@@ -0,0 +1,55 @@
+export function prim(graph: number[][], vertices: number): [number, number[][]] {
+  let cost = 0;
+  const mst: number[][] = [];
+
+  let count = vertices - 1;
+  const visited: boolean[] = new Array(vertices).fill(false);
+  const queue: number[][] = [];
+
+  for (const item of graph) {
+    if (item[0] === 0 || item[1] === 0) {
+      queue.push(item);
+    }
+  }
+  visited[0] = true;
+
+  while (count > 0) {
+    queue.sort((a, b) => a[2] - b[2]);
+    const [v, w, weight] = queue.shift()!;
+
+    if (!visited[v] || !visited[w]) {
+      mst.push([v, w]);
+      cost += weight;
+      count--;
+
+      const next_node = !visited[v] ? v : w;
+      visited[next_node] = true;
+
+      for (const item of graph) {
+        if ((item[0] === next_node && !visited[item[1]]) || (item[1] === next_node && !visited[item[0]])) {
+          queue.push(item);
+        }
+      }
+    }
+  }
+
+  return [cost, mst];
+}
+
+// test
+const res = prim(
+  [
+    [0, 1, 4],
+    [0, 2, 13],
+    [0, 3, 7],
+    [0, 4, 7],
+    [1, 2, 9],
+    [1, 3, 3],
+    [1, 4, 7],
+    [2, 3, 10],
+    [2, 4, 14],
+    [3, 4, 4],
+  ],
+  5
+);
+console.log(res);
