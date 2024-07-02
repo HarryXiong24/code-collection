6391ba3a67e9864f54f02ac68760b646f09f4696
harryxiong24 <harryxiong24@gmail.com>
2024-07-01 21:23:31 -0700

    2024.7.1

---
+++ Data Structure & Algorithm/Summary List/Code Random Thoughts/Two Pointer/344. Reverse String/344. Reverse String.ts
@@ -0,0 +1,34 @@
+// 344. Reverse String
+
+// Write a function that reverses a string. The input string is given as an array of characters s.
+
+// You must do this by modifying the input array in-place with O(1) extra memory.
+
+// Example 1:
+// Input: s = ["h","e","l","l","o"]
+// Output: ["o","l","l","e","h"]
+
+// Example 2:
+// Input: s = ["H","a","n","n","a","h"]
+// Output: ["h","a","n","n","a","H"]
+
+/**
+ Do not return anything, modify s in-place instead.
+ */
+export function reverseString(s: string[]): void {
+  let left = 0;
+  let right = s.length - 1;
+
+  while (left < right) {
+    const temp = s[left];
+    s[left] = s[right];
+    s[right] = temp;
+    left++;
+    right--;
+  }
+}
+
+// test
+const arr = ['h', 'e', 'l', 'l', 'o'];
+reverseString(arr);
+console.log(arr);
