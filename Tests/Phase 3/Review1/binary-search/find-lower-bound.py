6d28cf8303a4db5fe47f6af293738d6cb7063be3
harryxiong24 <harryxiong24@gmail.com>
2024-07-02 22:16:14 -0700

    2024.7.2

---
+++ Tests/Phase 3/Review1/binary-search/find-lower-bound.py
@@ -0,0 +1,24 @@
+from typing import List
+
+
+# Find Lower bound
+def binarySearch(nums: List[int], target: int) -> int:
+    left = 0
+    right = len(nums) - 1
+
+    while left <= right:
+        mid = left + (right - left) // 2
+        if target < nums[mid]:
+            right = mid - 1
+        else:
+            left = mid + 1
+
+    if left > 0 and nums[left - 1] == target:
+        return left - 1
+
+    return -1
+
+
+# test
+res = binarySearch([-1, 0, 3, 5, 9, 12], 9)
+print(res)
---
+++ Tests/Phase 3/Review1/binary-search/find-upper-bound.py
@@ -0,0 +1,24 @@
+from typing import List
+
+
+# Find Lower bound
+def binarySearch(nums: List[int], target: int) -> int:
+    left = 0
+    right = len(nums) - 1
+
+    while left <= right:
+        mid = left + (right - left) // 2
+        if target > nums[mid]:
+            left = mid + 1
+        else:
+            right = mid - 1
+
+    if right < len(nums) - 1 and nums[right + 1] == target:
+        return right + 1
+
+    return -1
+
+
+# test
+res = binarySearch([-1, 0, 3, 5, 9, 12], 9)
+print(res)
