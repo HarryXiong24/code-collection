d35ed67786b531c74b2a131e9cc5f7a4d5c738b0
harryxiong24 <harryxiong24@gmail.com>
2024-07-02 22:33:40 -0700

    2024.7.2

---
+++ Data Structure & Algorithm/Summary List/Code Random Thoughts/Two Pointer/142. Linked List Cycle II/142. Linked List Cycle II.ts
@@ -0,0 +1,57 @@
+// 142. Linked List Cycle II
+
+// Given the head of a linked list, return the node where the cycle begins. If there is no cycle, return null.
+
+// There is a cycle in a linked list if there is some node in the list that can be reached again by continuously following the next pointer. Internally, pos is used to denote the index of the node that tail's next pointer is connected to (0-indexed). It is -1 if there is no cycle. Note that pos is not passed as a parameter.
+
+// Do not modify the linked list.
+
+// Example 1:
+// Input: head = [3,2,0,-4], pos = 1
+// Output: tail connects to node index 1
+// Explanation: There is a cycle in the linked list, where tail connects to the second node.
+
+// Example 2:
+// Input: head = [1,2], pos = 0
+// Output: tail connects to node index 0
+// Explanation: There is a cycle in the linked list, where tail connects to the first node.
+
+// Example 3:
+// Input: head = [1], pos = -1
+// Output: no cycle
+// Explanation: There is no cycle in the linked list.
+
+// Definition for singly-linked list.
+class ListNode {
+  val: number;
+  next: ListNode | null;
+  constructor(val?: number, next?: ListNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.next = next === undefined ? null : next;
+  }
+}
+
+export function detectCycle(head: ListNode | null): ListNode | null {
+  if (!head) {
+    return null;
+  }
+
+  let fast: ListNode = head;
+  let slow: ListNode = head;
+
+  while (fast !== null && fast.next !== null) {
+    fast = fast.next!.next!;
+    slow = slow.next!;
+
+    if (fast === slow) {
+      slow = head;
+      while (fast !== slow) {
+        fast = fast.next!;
+        slow = slow.next!;
+      }
+      return slow;
+    }
+  }
+
+  return null;
+}
