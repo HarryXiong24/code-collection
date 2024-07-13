1d686dc449dc39374dac5872c697398f07dbe4a7
harryxiong24 <harryxiong24@gmail.com>
2024-07-12 20:48:52 -0700

    2024.7.12

---
+++ Tests/Phase 3/Review2/dijkstra/dijkstra.ts
@@ -0,0 +1,55 @@
+type EdgeMap = Record<string, number>;
+type Graph = Record<string, EdgeMap>;
+
+export function dijkstra(graph: Graph, start: string, end: string): [string[], number] {
+  const dijkstra_table = Object.keys(graph).reduce((obj, attr) => {
+    obj[attr] = Infinity;
+    return obj;
+  }, {} as EdgeMap);
+  const record_path = Object.keys(graph).reduce((obj, attr) => {
+    obj[attr] = null;
+    return obj;
+  }, {} as Record<string, string | null>);
+
+  // init
+  dijkstra_table[start] = 0;
+  const visited = new Set();
+  const queue: [string, number][] = [[start, 0]];
+
+  while (queue.length) {
+    queue.sort((a, b) => a[1] - b[1]);
+
+    const [current_node, current_cost] = queue.shift()!;
+
+    if (visited.has(current_node)) {
+      continue;
+    }
+    visited.add(current_node);
+
+    const neighbors = Object.keys(graph[current_node]);
+    for (const neighbor of neighbors) {
+      const weight: number = graph[current_node][neighbor];
+      if (current_cost + weight < dijkstra_table[neighbor]) {
+        dijkstra_table[neighbor] = current_cost + weight;
+        queue.push([neighbor, current_cost + weight]);
+        record_path[neighbor] = current_node;
+      }
+    }
+  }
+
+  const getShortPath = (node: string | null, result: string[]) => {
+    if (!node) {
+      return;
+    }
+    result.push(node);
+    getShortPath(record_path[node], result);
+  };
+
+  const shortest_path: string[] = [];
+  getShortPath(end, shortest_path);
+
+  return [shortest_path.reverse(), dijkstra_table[end]];
+}
+
+const [result, cost] = dijkstra(graph, 'A', 'E');
+console.log(result, cost);
