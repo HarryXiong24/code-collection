1293c460bd2e68ee8b85eb5026fb0f4660050d07
harryxiong24 <harryxiong24@gmail.com>
2024-07-20 15:16:24 -0700

    2024.7.20

---
+++ Data Structure & Algorithm/Summary List/Code Random Thoughts/Tree/226. Invert Binary Tree/226. Invert Binary Tree.ts
@@ -0,0 +1,50 @@
+// 226. Invert Binary Tree
+
+// Given the root of a binary tree, invert the tree, and return its root.
+
+// Example 1:
+// Input: root = [4,2,7,1,3,6,9]
+// Output: [4,7,2,9,6,3,1]
+
+// Example 2:
+// Input: root = [2,1,3]
+// Output: [2,3,1]
+
+// Example 3:
+// Input: root = []
+// Output: []
+
+// Definition for a binary tree node.
+class TreeNode {
+  val: number;
+  left: TreeNode | null;
+  right: TreeNode | null;
+  constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.left = left === undefined ? null : left;
+    this.right = right === undefined ? null : right;
+  }
+}
+
+export function invertTree(root: TreeNode | null): TreeNode | null {
+  if (!root) {
+    return null;
+  }
+
+  const recursion = (node: TreeNode | null) => {
+    if (!node) {
+      return null;
+    }
+
+    const temp = node.left;
+    node.left = node.right;
+    node.right = temp;
+
+    node.left = recursion(node.left);
+    node.right = recursion(node.right);
+
+    return node;
+  };
+
+  return recursion(root);
+}
