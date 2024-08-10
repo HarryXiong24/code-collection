dd7e139557979e15e7cf230a1697b5d420776854
harryxiong24 <harryxiong24@gmail.com>
2024-08-10 16:48:22 -0700

    2024.8.10

---
+++ Tests/Phase 4/Review2/monotonic-stack/monotonic-stack.ts
@@ -0,0 +1,18 @@
+function nextGreaterElement(nums: number[]): number[] {
+  const result: number[] = new Array(nums.length).fill(-1);
+  const monotonicStack: number[] = [];
+
+  for (let i = 0; i < nums.length; i++) {
+    while (monotonicStack.length && nums[monotonicStack[monotonicStack.length - 1]] < nums[i]) {
+      const top = monotonicStack.pop()!;
+      result[top] = nums[i];
+    }
+    monotonicStack.push(i);
+  }
+
+  return result;
+}
+
+// test
+const nums = [2, 1, 2, 4, 3];
+console.log(nextGreaterElement(nums)); // [4, 2, 4, -1, -1]
