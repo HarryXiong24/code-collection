0f7765c9d280b38762a6d6aad15ccaa78f23e795
harryxiong24 <harryxiong24@gmail.com>
2024-08-20 22:48:44 -0700

    2024.8.20

--- FrontEnd/Promise/promise.ts
+++ FrontEnd/Promise/promise.ts
@@ -1,138 +1,177 @@
-export class MyPromise {
+export class MyPromise<T> {
   private status: 'pending' | 'resolved' | 'rejected';
-  private data?: any;
+  private data?: T;
   private reason?: any;
-  private onResolvedCallback: ((...args: any[]) => any)[];
-  private onRejectedCallback: ((...args: any[]) => any)[];
+  private onResolvedCallback: ((...args: any[]) => void)[];
+  private onRejectedCallback: ((...args: any[]) => void)[];
 
-  constructor(executor: (resolve: (value: any) => void, reject: (reason: any) => void) => void) {
+  constructor(executor: (resolve: (value: T) => void, reject: (reason: any) => void) => void) {
     // Promise 当前的状态
     this.status = 'pending';
     // Promise 的值
     this.data = undefined;
     this.reason = undefined;
-    // Promise resolve 时的回调函数集，因为在Promise结束之前有可能有多个回调添加到它上面
+    // Promise resolve 时的回调函数集，因为在 Promise 结束之前有可能有多个回调添加到它上面
     this.onResolvedCallback = [];
-    // Promise reject 时的回调函数集，因为在Promise结束之前有可能有多个回调添加到它上面
+    // Promise reject 时的回调函数集，因为在 Promise 结束之前有可能有多个回调添加到它上面
     this.onRejectedCallback = [];
 
     if (typeof executor !== 'function') {
       throw TypeError('executor must be Function!');
     }
 
-    const resolve = (value: any) => {
+    const resolved = (value: T) => {
       if (this.status === 'pending') {
-        this.status = 'resolved';
-        this.data = value;
-        for (let i = 0; i < this.onResolvedCallback.length; i++) {
-          this.onResolvedCallback[i](value);
-        }
+        // use setTimeout to alter the Micro Task Queue, so the task will be input in Macro Task Queue
+        setTimeout(() => {
+          this.status = 'resolved';
+          this.data = value;
+          // 触发所有的 resolve 回调
+          this.onResolvedCallback.forEach((callback) => callback(value));
+        }, 0);
       }
     };
 
-    const reject = (reason: any) => {
+    const rejected = (reason: any) => {
       if (this.status === 'pending') {
-        this.status = 'rejected';
-        this.reason = reason;
-        for (var i = 0; i < this.onRejectedCallback.length; i++) {
-          this.onRejectedCallback[i](reason);
-        }
+        // use setTimeout to alter the Micro Task Queue, so the task will be input in Macro Task Queue
+        setTimeout(() => {
+          this.status = 'rejected';
+          this.reason = reason;
+          // 触发所有的 reject 回调
+          this.onRejectedCallback.forEach((callback) => callback(reason));
+        }, 0);
       }
     };
 
     try {
-      // 考虑到执行executor的过程中有可能出错，所以我们用try/catch块给包起来，并且在出错后以catch到的值reject掉这个Promise
-      executor.call(this, resolve, reject); // 执行executor
+      // 考虑到执行 executor 的过程中有可能出错，所以我们用 try/catch 块给包起来，并且在出错后以 catch 到的值reject 掉这个 Promise
+      executor.call(this, resolved, rejected); // 执行executor
     } catch (e) {
-      reject(e);
+      rejected(e);
     }
   }
 
   // then方法接收两个参数，onResolved，onRejected，分别为 Promise 成功或失败后的回调
-  then(onResolved?: ((value: any) => any) | null, onRejected?: ((reason: any) => any) | null) {
-    const self = this;
-
+  then(onResolved?: ((value: T) => any) | null, onRejected?: ((reason: any) => any) | null): MyPromise<T> {
     // 如果 then 的参数不是function，则我们需要忽略它
-    onResolved = typeof onResolved === 'function' ? onResolved : () => {};
-    onRejected = typeof onRejected === 'function' ? onRejected : () => {};
+    onResolved = typeof onResolved === 'function' ? onResolved : (value: T) => value;
+    onRejected =
+      typeof onRejected === 'function'
+        ? onRejected
+        : (reason: any) => {
+            throw reason;
+          };
 
-    return new MyPromise((resolve: (value: any) => void, reject: (reason: any) => void) => {
-      const fulfilled = () => {
-        try {
-          const x = onResolved && onResolved(self.data);
-          if (x && x instanceof Promise) {
-            // 如果 onResolved 的返回值是一个 Promise 对象，直接取它的结果做为结果
-            x.then(resolve, reject);
+    return new MyPromise<T>((resolve, reject) => {
+      const resolved = () => {
+        setTimeout(() => {
+          try {
+            const result = onResolved && onResolved(this.data!); // 调用 onResolved，并确保类型安全
+            this.resolvePromise(result, resolve, reject); // 如果返回值是一个 Promise，需要处理它
+          } catch (e) {
+            reject(e);
           }
-          // 否则，以它的返回值做为结果
-          resolve(x);
-        } catch (e) {
-          // 如果出错，以捕获到的错误做为结果
-          reject(e);
-        }
+        }, 0);
       };
 
       const rejected = () => {
-        try {
-          const x = onRejected && onRejected(self.reason);
-          if (x && x instanceof Promise) {
-            x.then(resolve, reject);
+        setTimeout(() => {
+          try {
+            const result = onRejected && onRejected(this.reason); // 调用 onRejected，并确保类型安全
+            this.resolvePromise(result, resolve, reject);
+          } catch (e) {
+            reject(e);
           }
-        } catch (e) {
-          reject(e);
-        }
+        }, 0);
       };
 
-      switch (self.status) {
+      switch (this.status) {
         case 'resolved':
-          fulfilled();
+          resolved();
           break;
         case 'rejected':
           rejected();
           break;
         default:
-          // self.status === 'pending'
-          // 如果当前的 Promise 还处于 pending 状态，我们并不能确定调用 onResolved 还是 onRejected，
-          // 只能等到 Promise 的状态确定后，才能确实如何处理。
-          // 所以我们需要把我们的两种情况的处理逻辑做为 callback 放入 promise (此处即this/self) 的回调数组里
-          // 逻辑本身跟第一个 if 块内的几乎一致
-          self.onResolvedCallback.push(fulfilled);
-          self.onRejectedCallback.push(rejected);
+          // demo case
+          // const pendingPromise = new MyPromise<number>((resolve, reject) => {
+          //   // 不调用 resolve 或 reject，Promise 会一直保持 pending 状态
+          //   console.log('Promise is pending...');
+          //   setTimeout(() => {
+          //     resolve(1);
+          //   }, 3000);
+          // });
+
+          // pendingPromise
+          //   .then((value) => {
+          //     console.log('This will not be called, because the promise is pending 1 :', value);
+          //     return value + 1;
+          //   })
+          //   .then((value) => {
+          //     console.log('This will not be called, because the promise is pending 2:', value);
+          //   })
+          //   .catch((reason) => {
+          //     console.log('This will not be called either, because the promise is pending:', reason);
+          //   });
+          this.onResolvedCallback.push(resolved);
+          this.onRejectedCallback.push(rejected);
           break;
       }
     });
   }
 
-  catch(onRejected: any) {
+  catch(onRejected: (reason: any) => void): MyPromise<T> {
+    // catch 实际上是调用 then 方法的简写版本，只处理错误情况
     return this.then(null, onRejected);
+  }
+
+  // 用于处理 then 中 onResolved 或 onRejected 返回值的逻辑
+  private resolvePromise(result: any, resolve: (value: T) => void, reject: (reason: any) => void) {
+    if (result === this) {
+      reject(new TypeError('Chaining cycle detected for promise'));
+    } else if (result && (typeof result === 'object' || typeof result === 'function')) {
+      let then = undefined;
+      try {
+        then = result.then;
+        if (typeof then === 'function') {
+          // demo case
+          // const myPromise = new MyPromise((resolve, reject) => {
+          //   const thenable = {
+          //     then: function (resolve: (value: any) => void) {
+          //       setTimeout(() => {
+          //         resolve("thenable object value");
+          //       }, 1000);
+          //     }
+          //   };
+          //   resolve(thenable);
+          // });
+
+          // myPromise.then((value) => {
+          //   console.log("Resolved with:", value);
+          // });
+          then.call(
+            result,
+            (y: any) => this.resolvePromise(y, resolve, reject),
+            (r: any) => reject(r)
+          );
+        } else {
+          resolve(result);
+        }
+      } catch (e) {
+        reject(e);
+      }
+    } else {
+      resolve(result);
+    }
   }
 }
 
 // test
 const a = 5;
 
-const myPromise = new MyPromise((resolve: (value: string) => void, reject) => {
-  if (a > 0) {
-    resolve('success');
-  } else {
-    reject('error');
-  }
-})
-  .then((value: string) => {
-    console.log('then1', value);
-    return value;
-  })
-  .then((value: string) => {
-    console.log('then2', value);
-  })
-  .then((value: any) => {
-    console.log('then3', value);
-  });
-
-console.log(myPromise);
-
 // test original promise
-console.log('------');
+console.log('Original Promise ------');
 const promise = new Promise((resolve: (value: string) => void, reject) => {
   if (a > 0) {
     resolve('success');
@@ -152,3 +191,25 @@ const promise = new Promise((resolve: (value: string) => void, reject) => {
   });
 
 console.log(promise);
+
+// test my promise
+console.log('My Promise ------');
+const myPromise = new MyPromise((resolve: (value: string) => void, reject) => {
+  if (a > 0) {
+    resolve('success');
+  } else {
+    reject('error');
+  }
+})
+  .then((value: string) => {
+    console.log('then1', value);
+    return value;
+  })
+  .then((value: string) => {
+    console.log('then2', value);
+  })
+  .then((value: any) => {
+    console.log('then3', value);
+  });
+
+console.log(myPromise);
