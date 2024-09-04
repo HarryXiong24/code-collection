3ec5b64a993ac611e47c33b4d1e8a3ff75688b24
harryxiong24 <harryxiong24@gmail.com>
2024-09-03 21:24:37 -0700

    2024.9.3

---
+++ Tests/Phase 4/Review4/heap-sort/heap-sort.ts
@@ -0,0 +1,37 @@
+function heapify(nums: number[], length: number, current_index: number) {
+  let max_index = current_index;
+  const left_index = 2 * current_index + 1;
+  const right_index = 2 * current_index + 2;
+
+  if (left_index < length && nums[max_index] < nums[left_index]) {
+    max_index = left_index;
+  }
+  if (right_index < length && nums[max_index] < nums[right_index]) {
+    max_index = right_index;
+  }
+
+  if (max_index !== current_index) {
+    const temp = nums[max_index];
+    nums[max_index] = nums[current_index];
+    nums[current_index] = temp;
+    heapify(nums, length, max_index);
+  }
+}
+
+export function heapSort(nums: number[]) {
+  for (let i = Math.floor(nums.length / 2) - 1; i >= 0; i--) {
+    heapify(nums, nums.length, i);
+  }
+
+  for (let i = nums.length - 1; i >= 0; i--) {
+    const temp = nums[0];
+    nums[0] = nums[i];
+    nums[i] = temp;
+    heapify(nums, i, 0);
+  }
+}
+
+// test
+const array = [2, 0, 2, 1, 1, 0, -3, -4];
+heapSort(array);
+console.log(array);
