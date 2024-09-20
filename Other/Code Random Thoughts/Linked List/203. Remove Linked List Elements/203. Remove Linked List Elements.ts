a38fb391563acb5651cd20ac18f510168e4a3091
harryxiong24 <harryxiong24@gmail.com>
2024-09-20 15:57:24 -0700

    2024.9.20

---
+++ Other/Code Random Thoughts/Linked List/203. Remove Linked List Elements/203. Remove Linked List Elements.ts
@@ -0,0 +1,44 @@
+// 203. Remove Linked List Elements
+
+// Given the head of a linked list and an integer val, remove all the nodes of the linked list that has Node.val == val, and return the new head.
+
+// Example 1:
+// Input: head = [1,2,6,3,4,5,6], val = 6
+// Output: [1,2,3,4,5]
+
+// Example 2:
+// Input: head = [], val = 1
+// Output: []
+
+// Example 3:
+// Input: head = [7,7,7,7], val = 7
+// Output: []
+
+// Definition for singly-linked list.
+class ListNode {
+  val: number;
+  next: ListNode | null;
+  constructor(val?: number, next?: ListNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.next = next === undefined ? null : next;
+  }
+}
+
+export function removeElements(head: ListNode | null, val: number): ListNode | null {
+  if (!head) {
+    return null;
+  }
+
+  const dummyNode = new ListNode(0, head);
+  let node = dummyNode;
+
+  while (node.next) {
+    if (node.next.val === val) {
+      node.next = node.next.next;
+    } else {
+      node = node.next;
+    }
+  }
+
+  return dummyNode.next;
+}
