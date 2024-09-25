f63b055bcecca38004fb59cea0bbc637fc8f6f9f
harryxiong24 <harryxiong24@gmail.com>
2024-09-25 10:55:01 -0700

    2024.9.25

---
+++ Other/Review/Phase 5/Review1/disjoint-set/disjoint-set.ts
@@ -0,0 +1,43 @@
+export class UnionFind {
+  root: number[];
+
+  constructor(size: number) {
+    this.root = new Array(size).fill(0).map((_i, index) => index);
+  }
+
+  find(x: number): number {
+    if (this.root[x] === x) {
+      return x;
+    }
+    return this.find(this.root[x]);
+  }
+
+  union(x: number, y: number) {
+    const root_x = this.find(x);
+    const root_y = this.find(y);
+
+    if (root_x !== root_y) {
+      this.root[root_y] = root_x;
+    }
+  }
+
+  connected(x: number, y: number) {
+    return this.find(x) === this.find(y);
+  }
+}
+
+// test
+const unionFind = new UnionFind(10);
+unionFind.union(1, 2);
+unionFind.union(2, 3);
+unionFind.union(4, 5);
+unionFind.union(6, 7);
+unionFind.union(8, 9);
+unionFind.union(1, 9);
+console.log(unionFind.connected(1, 9));
+console.log(unionFind.connected(3, 9));
+console.log(unionFind.connected(1, 4));
+console.log(unionFind.connected(8, 4));
+console.log(unionFind.connected(4, 5));
+console.log(unionFind.connected(5, 6));
+console.log(unionFind.connected(6, 7));
