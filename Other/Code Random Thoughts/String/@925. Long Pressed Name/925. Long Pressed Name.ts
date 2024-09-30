9284dbb33ccaf79fe38a6f85072871d70439e645
harryxiong24 <harryxiong24@gmail.com>
2024-09-29 22:19:29 -0700

    2024.9.29

---
+++ Other/Code Random Thoughts/String/@925. Long Pressed Name/925. Long Pressed Name.ts
@@ -0,0 +1,37 @@
+// 925. Long Pressed Name
+
+// Your friend is typing his name into a keyboard. Sometimes, when typing a character c, the key might get long pressed, and the character will be typed 1 or more times.
+
+// You examine the typed characters of the keyboard. Return True if it is possible that it was your friends name, with some characters (possibly none) being long pressed.
+
+// Example 1:
+// Input: name = "alex", typed = "aaleex"
+// Output: true
+// Explanation: 'a' and 'e' in 'alex' were long pressed.
+
+// Example 2:
+// Input: name = "saeed", typed = "ssaaedd"
+// Output: false
+// Explanation: 'e' must have been pressed twice, but it was not in the typed output.
+
+export function isLongPressedName(name: string, typed: string): boolean {
+  let i: number = 0;
+  let j: number = 0;
+
+  while (j < typed.length) {
+    if (name[i] === typed[j]) {
+      i++;
+      j++;
+    } else if (name[i] !== typed[j] && typed[j] === typed[j - 1]) {
+      j++;
+    } else {
+      return false;
+    }
+  }
+
+  return i === name.length && j === typed.length;
+}
+
+// test
+const res = isLongPressedName('saeed', 'ssaaedd');
+console.log(res);
