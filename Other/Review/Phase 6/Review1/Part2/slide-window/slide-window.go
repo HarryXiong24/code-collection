d4d105954ecd07d90808c9d0dfdf0f145171d032
harryxiong24 <harryxiong24@gmail.com>
2024-10-21 10:39:20 -0700

    2024.10.21

---
+++ Other/Review/Phase 6/Review1/Part2/slide-window/slide-window.go
@@ -0,0 +1,56 @@
+// 209. Minimum Size Subarray Sum
+
+// Given an array of positive integers nums and a positive integer target, return the minimal length of a subarray whose sum is greater than or equal to target. If there is no such subarray, return 0 instead.
+
+// Example 1:
+// Input: target = 7, nums = [2,3,1,2,4,3]
+// Output: 2
+// Explanation: The subarray [4,3] has the minimal length under the problem constraint.
+
+// Example 2:
+// Input: target = 4, nums = [1,4,4]
+// Output: 1
+
+// Example 3:
+// Input: target = 11, nums = [1,1,1,1,1,1,1,1]
+// Output: 0
+
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func minSubArrayLen(target int, nums []int) int {
+	fast := 0
+	slow := 0
+
+	res := math.MaxInt
+	sum := 0
+
+	for fast < len(nums) {
+		sum += nums[fast]
+
+		if sum >= target {
+			for sum-nums[slow] >= target {
+				sum -= nums[slow]
+				slow++
+			}
+			res = int(math.Min(float64(res), float64(fast-slow+1)))
+		}
+
+		fast++
+	}
+
+	if res == math.MaxInt {
+		return -1
+	}
+	return res
+}
+
+// test
+func main() {
+	res := minSubArrayLen(7, []int{2, 3, 1, 2, 4, 3})
+	fmt.Println(res) // 2
+}
