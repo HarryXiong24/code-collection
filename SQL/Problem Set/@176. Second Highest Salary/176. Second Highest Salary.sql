2ea1336214069c3345c3aa91f6944feda09c9edd
harryxiong24 <harryxiong24@gmail.com>
2024-10-29 21:37:13 -0700

    2024.10.29

---
+++ SQL/Problem Set/@176. Second Highest Salary/176. Second Highest Salary.sql
@@ -0,0 +1,53 @@
+-- 176. Second Highest Salary
+-- Table: Employee
+-- +-------------+------+
+-- | Column Name | Type |
+-- +-------------+------+
+-- | id          | int  |
+-- | salary      | int  |
+-- +-------------+------+
+-- id is the primary key (column with unique values) for this table.
+-- Each row of this table contains information about the salary of an employee.
+-- Write a solution to find the second highest distinct salary from the Employee table. If there is no second highest salary, return null (return None in Pandas).
+-- The result format is in the following example.
+-- Example 1:
+-- Input: 
+-- Employee table:
+-- +----+--------+
+-- | id | salary |
+-- +----+--------+
+-- | 1  | 100    |
+-- | 2  | 200    |
+-- | 3  | 300    |
+-- +----+--------+
+-- Output: 
+-- +---------------------+
+-- | SecondHighestSalary |
+-- +---------------------+
+-- | 200                 |
+-- +---------------------+
+-- Example 2:
+-- Input: 
+-- Employee table:
+-- +----+--------+
+-- | id | salary |
+-- +----+--------+
+-- | 1  | 100    |
+-- +----+--------+
+-- Output: 
+-- +---------------------+
+-- | SecondHighestSalary |
+-- +---------------------+
+-- | null                |
+-- +---------------------+
+select distinct
+  Max(salary) as SecondHighestSalary
+from
+  Employee
+where
+  salary < (
+    select
+      Max(salary) as SecondHighestSalary
+    from
+      Employee
+  )
