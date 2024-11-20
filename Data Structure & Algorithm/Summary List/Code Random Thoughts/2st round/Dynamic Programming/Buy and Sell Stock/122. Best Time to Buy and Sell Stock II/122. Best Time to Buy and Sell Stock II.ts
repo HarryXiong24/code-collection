6634934575491c085ac57f70b184d62d0dac126e
harryxiong24 <harryxiong24@gmail.com>
2024-11-20 15:31:31 -0800

    2024.11.20

---
+++ Data Structure & Algorithm/Summary List/Code Random Thoughts/2st round/Dynamic Programming/Buy and Sell Stock/122. Best Time to Buy and Sell Stock II/122. Best Time to Buy and Sell Stock II.ts
@@ -0,0 +1,44 @@
+// 122. Best Time to Buy and Sell Stock II
+
+// You are given an integer array prices where prices[i] is the price of a given stock on the ith day.
+
+// On each day, you may decide to buy and/or sell the stock. You can only hold at most one share of the stock at any time. However, you can buy it then immediately sell it on the same day.
+
+// Find and return the maximum profit you can achieve.
+
+// Example 1:
+// Input: prices = [7,1,5,3,6,4]
+// Output: 7
+// Explanation: Buy on day 2 (price = 1) and sell on day 3 (price = 5), profit = 5-1 = 4.
+// Then buy on day 4 (price = 3) and sell on day 5 (price = 6), profit = 6-3 = 3.
+// Total profit is 4 + 3 = 7.
+
+// Example 2:
+// Input: prices = [1,2,3,4,5]
+// Output: 4
+// Explanation: Buy on day 1 (price = 1) and sell on day 5 (price = 5), profit = 5-1 = 4.
+// Total profit is 4.
+
+// Example 3:
+// Input: prices = [7,6,4,3,1]
+// Output: 0
+// Explanation: There is no way to make a positive profit, so we never buy the stock to achieve the maximum profit of 0.
+
+export function maxProfit(prices: number[]): number {
+  // dp[i][0] 持有股票 i 最大金额, dp[i][1] 不持有股票 i 最大金额
+  const dp: number[][] = new Array(prices.length).fill(0).map(() => new Array(2).fill(0));
+
+  dp[0][0] = -prices[0];
+  dp[0][1] = 0;
+
+  for (let i = 1; i < prices.length; i++) {
+    dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] - prices[i]);
+    dp[i][1] = Math.max(dp[i - 1][1], dp[i - 1][0] + prices[i]);
+  }
+
+  return Math.max(dp[prices.length - 1][0], dp[prices.length - 1][1]);
+}
+
+// test
+const res = maxProfit([7, 1, 5, 3, 6, 4]);
+console.log(res);
