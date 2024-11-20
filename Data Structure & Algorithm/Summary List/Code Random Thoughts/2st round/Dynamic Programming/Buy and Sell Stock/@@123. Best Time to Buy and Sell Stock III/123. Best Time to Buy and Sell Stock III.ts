8882027f00b8dd497581dc2c382656b6e8823d24
harryxiong24 <harryxiong24@gmail.com>
2024-11-20 15:54:30 -0800

    2024.11.20

---
+++ Data Structure & Algorithm/Summary List/Code Random Thoughts/2st round/Dynamic Programming/Buy and Sell Stock/@@123. Best Time to Buy and Sell Stock III/123. Best Time to Buy and Sell Stock III.ts
@@ -0,0 +1,53 @@
+// 123. Best Time to Buy and Sell Stock III
+
+// You are given an array prices where prices[i] is the price of a given stock on the ith day.
+
+// Find the maximum profit you can achieve. You may complete at most two transactions.
+
+// Note: You may not engage in multiple transactions simultaneously (i.e., you must sell the stock before you buy again).
+
+// Example 1:
+// Input: prices = [3,3,5,0,0,3,1,4]
+// Output: 6
+// Explanation: Buy on day 4 (price = 0) and sell on day 6 (price = 3), profit = 3-0 = 3.
+// Then buy on day 7 (price = 1) and sell on day 8 (price = 4), profit = 4-1 = 3.
+
+// Example 2:
+// Input: prices = [1,2,3,4,5]
+// Output: 4
+// Explanation: Buy on day 1 (price = 1) and sell on day 5 (price = 5), profit = 5-1 = 4.
+// Note that you cannot buy on day 1, buy on day 2 and sell them later, as you are engaging multiple transactions at the same time. You must sell before buying again.
+
+// Example 3:
+// Input: prices = [7,6,4,3,1]
+// Output: 0
+// Explanation: In this case, no transaction is done, i.e. max profit = 0.
+
+export function maxProfit(prices: number[]): number {
+  // dp[i][0] 没有操作 （其实我们也可以不设置这个状态）
+  // dp[i][1] 第一次持有股票
+  // dp[i][2] 第一次不持有股票
+  // dp[i][3] 第二次持有股票
+  // dp[i][4] 第二次不持有股票
+  const dp: number[][] = new Array(prices.length).fill(0).map(() => new Array(2).fill(0));
+
+  dp[0][0] = 0;
+  dp[0][1] = -prices[0];
+  dp[0][2] = 0;
+  dp[0][3] = -prices[0];
+  dp[0][4] = 0;
+
+  for (let i = 1; i < prices.length; i++) {
+    dp[i][0] = dp[i - 1][0];
+    dp[i][1] = Math.max(dp[i - 1][1], dp[i - 1][0] - prices[i]);
+    dp[i][2] = Math.max(dp[i - 1][2], dp[i - 1][1] + prices[i]);
+    dp[i][3] = Math.max(dp[i - 1][3], dp[i - 1][2] - prices[i]);
+    dp[i][4] = Math.max(dp[i - 1][4], dp[i - 1][3] + prices[i]);
+  }
+
+  return Math.max(dp[prices.length - 1][4]);
+}
+
+// test
+const res = maxProfit([3, 3, 5, 0, 0, 3, 1, 4]);
+console.log(res);
