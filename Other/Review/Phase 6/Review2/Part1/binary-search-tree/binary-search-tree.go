a6de79bd6318d2d24e6adbf4574d610613fb5ef3
harryxiong24 <harryxiong24@gmail.com>
2024-11-21 11:15:36 -0800

    2024.11.21

---
+++ Other/Review/Phase 6/Review2/Part1/binary-search-tree/binary-search-tree.go
@@ -0,0 +1,208 @@
+package main
+
+import "fmt"
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+type BinarySearchTree struct {
+	root *TreeNode
+}
+
+// NewTreeNode is a constructor for TreeNode
+func NewTreeNode(val int) *TreeNode {
+	return &TreeNode{Val: val, Left: nil, Right: nil}
+}
+
+// NewBinarySearchTree is a constructor for BinarySearchTree
+func NewBinarySearchTree(node *TreeNode) *BinarySearchTree {
+	return &BinarySearchTree{root: node}
+}
+
+func (bst *BinarySearchTree) SearchNode(value int) bool {
+	var recursive func(node *TreeNode) bool
+	recursive = func(node *TreeNode) bool {
+		if node == nil {
+			return false
+		}
+
+		if value < node.Val {
+			return recursive(node.Left)
+		} else if value > node.Val {
+			return recursive(node.Right)
+		} else {
+			return true
+		}
+	}
+
+	return recursive(bst.root)
+}
+
+func (bst *BinarySearchTree) InsertNode(newValue int) bool {
+
+	if bst.root == nil {
+		bst.root = NewTreeNode(newValue)
+		return true
+	}
+
+	var recursive func(node *TreeNode) bool
+	recursive = func(node *TreeNode) bool {
+
+		if newValue < node.Val {
+			if node.Left == nil {
+				node.Left = NewTreeNode(newValue)
+				return true
+			}
+			return recursive(node.Left)
+		} else if newValue > node.Val {
+			if node.Right == nil {
+				node.Right = NewTreeNode(newValue)
+				return true
+			}
+			return recursive(node.Right)
+		} else {
+			return false
+		}
+	}
+
+	return recursive(bst.root)
+}
+
+func (bst *BinarySearchTree) DeleteNode(value int) bool {
+	var recursive func(node *TreeNode, parent *TreeNode) bool
+	recursive = func(node *TreeNode, parent *TreeNode) bool {
+
+		if node == nil {
+			return false
+		}
+
+		if value < node.Val {
+			return recursive(node.Left, node)
+		} else if value > node.Val {
+			return recursive(node.Right, node)
+		} else {
+			if node.Left == nil || node.Right == nil {
+				var nextChild *TreeNode
+				if node.Left != nil {
+					nextChild = node.Left
+				} else {
+					nextChild = node.Right
+				}
+
+				if parent == nil {
+					bst.root = nextChild
+				} else if parent.Left == node {
+					parent.Left = nextChild
+				} else {
+					parent.Right = nextChild
+				}
+			} else {
+				curMinNode := node.Right
+				parentNode := node
+
+				for curMinNode.Left != nil {
+					parentNode = curMinNode
+					curMinNode = curMinNode.Left
+				}
+
+				node.Val = curMinNode.Val
+
+				if parentNode.Left == curMinNode {
+					parentNode.Left = curMinNode.Right
+				} else {
+					parentNode.Right = curMinNode.Right
+				}
+			}
+
+			return true
+		}
+
+	}
+
+	return recursive(bst.root, nil)
+}
+
+func (bst *BinarySearchTree) Preorder() []int {
+	res := make([]int, 0)
+
+	var recursive func(node *TreeNode)
+	recursive = func(node *TreeNode) {
+		if node == nil {
+			return
+		}
+
+		res = append(res, node.Val)
+		recursive(node.Left)
+		recursive(node.Right)
+	}
+
+	recursive(bst.root)
+	return res
+}
+
+func (bst *BinarySearchTree) Inorder() []int {
+	res := make([]int, 0)
+
+	var recursive func(node *TreeNode)
+	recursive = func(node *TreeNode) {
+		if node == nil {
+			return
+		}
+
+		recursive(node.Left)
+		res = append(res, node.Val)
+		recursive(node.Right)
+	}
+
+	recursive(bst.root)
+	return res
+}
+
+func (bst *BinarySearchTree) Postorder() []int {
+	res := make([]int, 0)
+
+	var recursive func(node *TreeNode)
+	recursive = func(node *TreeNode) {
+		if node == nil {
+			return
+		}
+
+		recursive(node.Left)
+		recursive(node.Right)
+		res = append(res, node.Val)
+	}
+
+	recursive(bst.root)
+	return res
+}
+
+// test
+func main() {
+	tree := NewBinarySearchTree(nil)
+	tree.InsertNode(8)
+	tree.InsertNode(14)
+	tree.InsertNode(10)
+	tree.InsertNode(13)
+	tree.InsertNode(3)
+	tree.InsertNode(1)
+	tree.InsertNode(6)
+	tree.InsertNode(4)
+	tree.InsertNode(7)
+	insert_res1 := tree.Preorder()
+	fmt.Println(insert_res1)
+	search_res1 := tree.SearchNode(14)
+	fmt.Println(search_res1)
+	search_res2 := tree.SearchNode(5)
+	fmt.Println(search_res2)
+
+	tree.DeleteNode(8)
+	delete_res1 := tree.Preorder()
+	fmt.Println(delete_res1)
+
+	tree.DeleteNode(3)
+	delete_res2 := tree.Preorder()
+	fmt.Println(delete_res2)
+}
