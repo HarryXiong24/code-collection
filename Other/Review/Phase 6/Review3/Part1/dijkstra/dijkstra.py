73be6506bb11e8d9a6d53cf2eae44181231c5868
harryxiong24 <harryxiong24@gmail.com>
2024-12-04 11:05:20 -0800

    2024.12.4

---
+++ Other/Review/Phase 6/Review3/Part1/dijkstra/dijkstra.py
@@ -0,0 +1,53 @@
+from typing import List
+
+
+def dijkstra(
+    graph: dict[str, dict[str, float]], start: int, end: int
+) -> tuple[dict[str, float], int]:
+    dijkstra_table: dict[str, float] = {node: float("infinity") for node in graph}
+    record_path: dict[str, str] = {node: None for node in graph}
+    visited = set({})
+
+    # init
+    queue: List[List[str, int]] = [[start, 0]]
+    dijkstra_table[start] = 0
+
+    while len(queue):
+        queue.sort(key=lambda x: x[1])
+        current_node, current_cost = queue.pop(0)
+
+        if current_node in visited:
+            continue
+        visited.add(current_node)
+
+        neighbors = graph[current_node]
+        for neighbor in neighbors:
+            weight = graph[current_node][neighbor]
+            if current_cost + weight < dijkstra_table[neighbor]:
+                dijkstra_table[neighbor] = current_cost + weight
+                queue.append([neighbor, current_cost + weight])
+                record_path[neighbor] = current_node
+
+    def getShortestPath(node: str | None, result: List[str]):
+        if not node:
+            return
+        result.append(node)
+        getShortestPath(record_path[node], result)
+
+    shortest_path: List[str] = []
+    getShortestPath(end, shortest_path)
+    shortest_path.reverse()
+
+    return shortest_path, dijkstra_table[end]
+
+
+# test
+graph = {
+    "A": {"B": 1, "C": 1, "D": 3},
+    "B": {"A": 1, "D": 2, "E": 1},
+    "C": {"A": 1, "D": 1},
+    "D": {"A": 2, "B": 3, "C": 1, "E": 2},
+    "E": {"B": 1, "D": 2},
+}
+result, cost = dijkstra(graph, "A", "E")
+print(result, cost)
