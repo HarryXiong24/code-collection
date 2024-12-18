b6cf823c3eb680c90b197ba29d8aad14010c5c3e
harryxiong24 <harryxiong24@gmail.com>
2024-12-17 22:47:42 -0800

    2024.12.17

---
+++ Backend/Golang/tricky-cases/19/main.go
@@ -0,0 +1,23 @@
+package main
+
+type S struct {
+}
+
+func f(x interface{}) {
+}
+
+func g(x *interface{}) {
+}
+
+func main() {
+	s := S{}
+	p := &s
+	f(s) //A
+	g(s) //B
+	f(p) //C
+	g(p) //D
+}
+
+// 函数参数为 interface{} 时可以接收任何类型的参数，包括用户自定义类型等，即使是接收指针类型也用 interface{}，而不是使用 *interface{}。
+
+// 永远不要使用一个指针指向一个接口类型，因为它已经是一个指针。
---
+++ Backend/Golang/tricky-cases/20/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+var a bool = true
+
+func main() {
+	defer func() {
+		fmt.Println("1")
+	}()
+	if a == true {
+		fmt.Println("2")
+		return
+	}
+	defer func() {
+		fmt.Println("3")
+	}()
+}
+
+// 2 1
+// defer 关键字后面的函数或者方法想要执行必须先注册，return 之后的 defer 是不能注册的， 也就不能执行后面的函数或方法
