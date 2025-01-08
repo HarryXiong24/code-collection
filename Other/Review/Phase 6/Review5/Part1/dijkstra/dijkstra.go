b6dba8efaea3e5960cabbdcb120a1a01ccb4f090
harryxiong24 <harryxiong24@gmail.com>
2025-01-08 11:29:29 -0800

    2024.1.8

---
+++ Other/Review/Phase 6/Review5/Part1/dijkstra/dijkstra.go
@@ -0,0 +1,92 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"sort"
+)
+
+// EdgeMap represents the neighbors and their corresponding weights
+type EdgeMap map[string]int
+
+// Graph represents the whole graph where each node maps to its neighbors
+type Graph map[string]EdgeMap
+
+// queue's element
+type Item struct {
+	node string
+	cost int
+}
+
+func dijkstra(graph Graph, start string, end string) ([]string, int) {
+	dijkstraTable := make(EdgeMap)
+	recordPath := make(map[string]string)
+
+	for key := range graph {
+		dijkstraTable[key] = math.MaxInt
+		recordPath[key] = ""
+	}
+
+	// init
+	visited := make(map[string]bool)
+	queue := make([]*Item, 0)
+
+	dijkstraTable[start] = 0
+	queue = append(queue, &Item{start, 0})
+
+	for len(queue) > 0 {
+		sort.Slice(queue, func(i, j int) bool {
+			return queue[i].cost < queue[j].cost
+		})
+
+		current := queue[0]
+		queue = queue[1:]
+
+		currentNode, currentCost := current.node, current.cost
+
+		if _, exist := visited[currentNode]; exist {
+			continue
+		}
+		visited[currentNode] = true
+
+		for neighbor, weight := range graph[currentNode] {
+			if currentCost+weight < dijkstraTable[neighbor] {
+				dijkstraTable[neighbor] = currentCost + weight
+				recordPath[neighbor] = currentNode
+				queue = append(queue, &Item{neighbor, currentCost + weight})
+			}
+		}
+	}
+
+	shortestPath := []string{}
+	var getShortestPath func(node string)
+	getShortestPath = func(node string) {
+		if node == "" {
+			return
+		}
+		shortestPath = append(shortestPath, node)
+		getShortestPath(recordPath[node])
+	}
+
+	getShortestPath(end)
+
+	for i, j := 0, len(shortestPath)-1; i < j; i, j = i+1, j-1 {
+		shortestPath[i], shortestPath[j] = shortestPath[j], shortestPath[i]
+	}
+
+	return shortestPath, dijkstraTable[end]
+}
+
+// test
+func main() {
+	graph := Graph{
+		"A": {"B": 1, "C": 1, "D": 3},
+		"B": {"A": 1, "D": 2, "E": 1},
+		"C": {"A": 1, "D": 1},
+		"D": {"A": 3, "B": 2, "C": 1, "E": 2},
+		"E": {"B": 1, "D": 2},
+	}
+
+	result, cost := dijkstra(graph, "A", "E")
+	fmt.Println("Shortest path:", result, "Cost:", cost)
+}
