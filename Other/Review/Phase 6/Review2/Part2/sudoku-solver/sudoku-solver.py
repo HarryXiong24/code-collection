eb6505a66ccc622b6b72a8647050686d72963bac
harryxiong24 <harryxiong24@gmail.com>
2025-01-16 16:05:21 -0800

    2025.1.16

--- Other/Review/Phase 6/Review2/Part2/sudoku-solver/sudoku-solver.py
+++ Other/Review/Phase 6/Review2/Part2/sudoku-solver/sudoku-solver.py
@@ -13,3 +13,64 @@
 # Input: board = [["5","3",".",".","7",".",".",".","."],["6",".",".","1","9","5",".",".","."],[".","9","8",".",".",".",".","6","."],["8",".",".",".","6",".",".",".","3"],["4",".",".","8",".","3",".",".","1"],["7",".",".",".","2",".",".",".","6"],[".","6",".",".",".",".","2","8","."],[".",".",".","4","1","9",".",".","5"],[".",".",".",".","8",".",".","7","9"]]
 # Output: [["5","3","4","6","7","8","9","1","2"],["6","7","2","1","9","5","3","4","8"],["1","9","8","3","4","2","5","6","7"],["8","5","9","7","6","1","4","2","3"],["4","2","6","8","5","3","7","9","1"],["7","1","3","9","2","4","8","5","6"],["9","6","1","5","3","7","2","8","4"],["2","8","7","4","1","9","6","3","5"],["3","4","5","2","8","6","1","7","9"]]
 # Explanation: The input board is shown above and the only valid solution is shown below:
+
+
+from typing import List
+
+
+class Solution:
+    def solveSudoku(self, board: List[List[str]]) -> None:
+        """
+        Do not return anything, modify board in-place instead.
+        """
+
+        def is_valid(row: int, col: int, char: str, board: list[list[str]]) -> bool:
+            for i in range(9):
+                if board[row][i] == char:
+                    return False
+
+            for i in range(9):
+                if board[i][col] == char:
+                    return False
+
+            start_row = (row // 3) * 3
+            start_col = (col // 3) * 3
+            for i in range(start_row, start_row + 3):
+                for j in range(start_col, start_col + 3):
+                    if board[i][j] == char:
+                        return False
+
+            return True
+
+        def backtrack(board: list[list[str]]) -> bool:
+            for i in range(9):
+                for j in range(9):
+                    if board[i][j] == ".":
+                        for char in map(str, range(1, 10)):  # '1' ~ '9'
+                            if is_valid(i, j, char, board):
+                                board[i][j] = char
+                                if backtrack(board):
+                                    return True
+                                board[i][j] = "."
+                        return False
+            return True
+
+        backtrack(board)
+
+
+# test
+board = [
+    ["5", "3", ".", ".", "7", ".", ".", ".", "."],
+    ["6", ".", ".", "1", "9", "5", ".", ".", "."],
+    [".", "9", "8", ".", ".", ".", ".", "6", "."],
+    ["8", ".", ".", ".", "6", ".", ".", ".", "3"],
+    ["4", ".", ".", "8", ".", "3", ".", ".", "1"],
+    ["7", ".", ".", ".", "2", ".", ".", ".", "6"],
+    [".", "6", ".", ".", ".", ".", "2", "8", "."],
+    [".", ".", ".", "4", "1", "9", ".", ".", "5"],
+    [".", ".", ".", ".", "8", ".", ".", "7", "9"],
+]
+
+solution = Solution()
+solution.solveSudoku(board)
+print(board)
