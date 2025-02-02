f905c7cfaf7279321793c0dbc6bea2e097758ed3
harryxiong24 <harryxiong24@gmail.com>
2025-02-01 22:08:33 -0800

    2025.2.1

---
+++ Data Structure & Algorithm/Summary List/Code Random Thoughts/3rd round/Array/Binary Search Series/704. Binary Search/704. Binary Search.ts
@@ -0,0 +1,37 @@
+// 704. Binary Search
+
+// Given an array of integers nums which is sorted in ascending order, and an integer target, write a function to search target in nums. If target exists, then return its index. Otherwise, return -1.
+
+// You must write an algorithm with O(log n) runtime complexity.
+
+// Example 1:
+// Input: nums = [-1,0,3,5,9,12], target = 9
+// Output: 4
+// Explanation: 9 exists in nums and its index is 4
+
+// Example 2:
+// Input: nums = [-1,0,3,5,9,12], target = 2
+// Output: -1
+// Explanation: 2 does not exist in nums so return -1
+
+export function search(nums: number[], target: number): number {
+  let left = 0;
+  let right = nums.length - 1;
+
+  while (left <= right) {
+    const mid = left + Math.floor((right - left) / 2);
+    if (target < nums[mid]) {
+      right = mid - 1;
+    } else if (target > nums[mid]) {
+      left = mid + 1;
+    } else {
+      return mid;
+    }
+  }
+
+  return -1;
+}
+
+// test
+const res = search([-1, 0, 3, 5, 9, 12], 9);
+console.log(res);
