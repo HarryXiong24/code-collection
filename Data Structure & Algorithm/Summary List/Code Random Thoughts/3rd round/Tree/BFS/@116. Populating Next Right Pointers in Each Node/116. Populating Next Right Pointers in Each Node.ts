5a23e3cd7245368e0fdb560b669adf34cea48589
harryxiong24 <harryxiong24@gmail.com>
2025-06-11 22:43:14 -0700

    2025.6.11

---
+++ Data Structure & Algorithm/Summary List/Code Random Thoughts/3rd round/Tree/BFS/@116. Populating Next Right Pointers in Each Node/116. Populating Next Right Pointers in Each Node.ts
@@ -0,0 +1,65 @@
+// 116. Populating Next Right Pointers in Each Node
+
+// You are given a perfect binary tree where all leaves are on the same level, and every parent has two children. The binary tree has the following definition:
+
+// struct Node {
+//   int val;
+//   Node *left;
+//   Node *right;
+//   Node *next;
+// }
+// Populate each next pointer to point to its next right node. If there is no next right node, the next pointer should be set to NULL.
+
+// Initially, all next pointers are set to NULL.
+
+// Example 1:
+// Input: root = [1,2,3,4,5,6,7]
+// Output: [1,#,2,3,#,4,5,6,7,#]
+// Explanation: Given the above perfect binary tree (Figure A), your function should populate each next pointer to point to its next right node, just like in Figure B. The serialized output is in level order as connected by the next pointers, with '#' signifying the end of each level.
+
+// Example 2:
+// Input: root = []
+// Output: []
+
+// Definition for _Node.
+class _Node {
+  val: number;
+  left: _Node | null;
+  right: _Node | null;
+  next: _Node | null;
+  constructor(val?: number, left?: _Node, right?: _Node, next?: _Node) {
+    this.val = val === undefined ? 0 : val;
+    this.left = left === undefined ? null : left;
+    this.right = right === undefined ? null : right;
+    this.next = next === undefined ? null : next;
+  }
+}
+
+export function connect(root: _Node | null): _Node | null {
+  const queue: _Node[] = [];
+
+  if (!root) {
+    return null;
+  }
+
+  queue.push(root);
+
+  while (queue.length) {
+    const size = queue.length;
+
+    for (let i = 0; i < size; i++) {
+      const current = queue.shift()!;
+
+      if (i === size - 1) {
+        current.next = null;
+      } else {
+        current.next = queue[0];
+      }
+
+      current.left && queue.push(current.left);
+      current.right && queue.push(current.right);
+    }
+  }
+
+  return root;
+}
