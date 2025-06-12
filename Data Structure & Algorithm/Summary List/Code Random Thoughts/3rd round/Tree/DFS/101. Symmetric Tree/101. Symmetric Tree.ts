f7a9a0a7295d131d24f4cb70caae19b3586abb79
harryxiong24 <harryxiong24@gmail.com>
2025-06-11 23:24:10 -0700

    2025.6.11

---
+++ Data Structure & Algorithm/Summary List/Code Random Thoughts/3rd round/Tree/DFS/101. Symmetric Tree/101. Symmetric Tree.ts
@@ -0,0 +1,47 @@
+// 101. Symmetric Tree
+
+// Given the root of a binary tree, check whether it is a mirror of itself (i.e., symmetric around its center).
+
+// Example 1:
+// Input: root = [1,2,2,3,4,4,3]
+// Output: true
+
+// Example 2:
+// Input: root = [1,2,2,null,3,null,3]
+// Output: false
+
+// Definition for a binary tree node.
+class TreeNode {
+  val: number;
+  left: TreeNode | null;
+  right: TreeNode | null;
+  constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.left = left === undefined ? null : left;
+    this.right = right === undefined ? null : right;
+  }
+}
+
+export function isSymmetric(root: TreeNode | null): boolean {
+  if (!root) {
+    return true;
+  }
+
+  const recursion = (left: TreeNode | null, right: TreeNode | null): boolean => {
+    if (!left && !right) {
+      return true;
+    } else if (left && !right) {
+      return false;
+    } else if (!left && right) {
+      return false;
+    } else {
+      if (left!.val !== right!.val) {
+        return false;
+      } else {
+        return recursion(left!.left, right!.right) && recursion(left!.right, right!.left);
+      }
+    }
+  };
+
+  return recursion(root.left, root.right);
+}
