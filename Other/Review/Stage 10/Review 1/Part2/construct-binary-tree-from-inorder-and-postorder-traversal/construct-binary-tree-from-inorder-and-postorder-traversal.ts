818d47b26cef05dd5f72af288e99d2e2a4b948b1
harryxiong24 <harryxiong24@gmail.com>
2025-08-22 10:43:58 -0700

    2025.8.22

---
+++ Other/Review/Stage 10/Review 1/Part2/construct-binary-tree-from-inorder-and-postorder-traversal/construct-binary-tree-from-inorder-and-postorder-traversal.ts
@@ -0,0 +1,42 @@
+// 106. Construct Binary Tree from Inorder and Postorder Traversal
+
+// Given two integer arrays inorder and postorder where inorder is the inorder traversal of a binary tree and postorder is the postorder traversal of the same tree, construct and return the binary tree.
+
+// Example 1:
+// Input: inorder = [9,3,15,20,7], postorder = [9,15,7,20,3]
+// Output: [3,9,20,null,null,15,7]
+
+// Example 2:
+// Input: inorder = [-1], postorder = [-1]
+// Output: [-1]
+
+// Definition for a binary tree node.
+class TreeNode {
+  val: number;
+  left: TreeNode | null;
+  right: TreeNode | null;
+  constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.left = left === undefined ? null : left;
+    this.right = right === undefined ? null : right;
+  }
+}
+
+export function buildTree(inorder: number[], postorder: number[]): TreeNode | null {
+  const recursive = (inorder: number[], postorder: number[]) => {
+    if (!inorder.length || !postorder.length) {
+      return null;
+    }
+
+    const currentVal = postorder.pop()!;
+    const node = new TreeNode(currentVal);
+    const index = inorder.indexOf(currentVal);
+
+    node.right = recursive(inorder.slice(index + 1), postorder);
+    node.left = recursive(inorder.slice(0, index), postorder);
+
+    return node;
+  };
+
+  return recursive(inorder, postorder);
+}
