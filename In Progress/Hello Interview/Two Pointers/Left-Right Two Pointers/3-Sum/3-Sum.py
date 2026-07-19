2a37003b9c4f02cb8fb61b9ae1ae6555cc88768d
harryxiong24 <harryxiong24@gmail.com>
2026-07-18 21:33:01 -0700

    2026.7.18

---
+++ In Progress/Hello Interview/Two Pointers/Left-Right Two Pointers/3-Sum/3-Sum.py
@@ -0,0 +1,50 @@
+# DESCRIPTION (inspired by Leetcode.com)
+# Given an input integer array nums, write a function to find all unique triplets [nums[i], nums[j], nums[k]] such that i, j, and k are distinct indices, and the sum of nums[i], nums[j], and nums[k] equals zero. Ensure that the resulting list does not contain any duplicate triplets.
+
+# Input:
+# nums = [-1,0,1,2,-1,-1]
+# Output:
+# [[-1,-1,2],[-1,0,1]]
+
+# Explanation: Both nums[0], nums[1], nums[2] and nums[1], nums[2], nums[4] both include [-1, 0, 1] and sum to 0. nums[0], nums[3], nums[4] ([-1,-1,2]) also sum to 0.
+# Since we are looking for unique triplets, we can ignore the duplicate [-1, 0, 1] triplet and return [[-1, -1, 2], [-1, 0, 1]].
+# The order of the triplets and the order of the elements within the triplets do not matter.
+
+
+class Solution:
+    def threeSum(self, nums: list[int]) -> list[list[int]]:
+        res: list[list[int]] = []
+
+        nums.sort()
+
+        # Your code goes here
+        for i in range(0, len(nums)):
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+
+            left = i + 1
+            right = len(nums) - 1
+
+            while left < right:
+                if nums[i] + nums[left] + nums[right] < 0:
+                    left += 1
+                elif nums[i] + nums[left] + nums[right] > 0:
+                    right -= 1
+                else:
+                    res.append([nums[i], nums[left], nums[right]])
+
+                    while left < right and nums[left] == nums[left + 1]:
+                        left += 1
+
+                    while left < right and nums[right] == nums[right - 1]:
+                        right -= 1
+
+                    left += 1
+                    right -= 1
+
+        return res
+
+
+# test
+res = Solution().threeSum([-1, 0, 1, 2, -1, -1])
+print(res)
