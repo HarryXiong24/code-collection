b4c799ef83b72adde42c067c207d52ca5688ecd0
harryxiong24 <harryxiong24@gmail.com>
2026-07-18 21:14:48 -0700

    2026.7.18

---
+++ In Progress/Hello Interview/Two Pointers/Left-Right Two Pointers/Two Sum (Sorted Array)/Two Sum (Sorted Array).py
@@ -0,0 +1,38 @@
+# DESCRIPTION (inspired by Leetcode.com)
+# Given a sorted array of integers nums, determine if there exists a pair of numbers that sum to a given target.
+
+# Example 1:
+# Input:
+# nums = [1,3,4,6,8,10,13]
+# target = 13
+# Output:
+# True # (3 + 10 = 13)
+
+# Example 2:
+# Input:
+# nums = [1,3,4,6,8,10,13]
+# target = 6
+# Output:
+# False
+
+
+class Solution:
+    def twoSum(self, nums: list[int], target: int) -> bool:
+        # Your code goes here
+        left = 0
+        right = len(nums) - 1
+
+        while left < right:
+            if nums[left] + nums[right] < target:
+                left += 1
+            elif nums[left] + nums[right] > target:
+                right -= 1
+            else:
+                return True
+
+        return False
+
+
+# test
+res = Solution().twoSum([1, 3, 4, 6, 8, 10, 13], 6)
+print(res)
